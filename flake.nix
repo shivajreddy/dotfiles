cbdbcbbd8f8f450bc2dbcff7e08c035f01a8b05c
shivajreddy <shivajreddy@outlook.com>
2024-03-04 10:47:29 -0500

    setting up my first nixos

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "My Flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    # ------------ HomeManager module IMPORT ------------ #
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  # outputs = { self, nixpkgs, ... }:
+  outputs = inputs@{ self, nixpkgs, ... }:
+  # outputs = inputs@{ self, nixpkgs, home-manager, ... }:
+  # outputs = { self, nixpkgs, home-manager, ... }:
+  let 
+        lib = nixpkgs.lib; 
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    # -------------------- HOME config -------------------- #.
+
+    nixosConfigurations = {
+      predator = lib.nixosSystem {
+        # extraSpecialArgs = { inherit inputs; };
+        inherit system;
+        modules = [ 
+          ./configuration.nix 
+          # inputs.home-manager.nixosModules.home-manager
+          # /*
+          inputs.home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.shiva = import ./home.nix;
+          }
+          # */
+        ];
+      };
+    };
+
+    /* HomeConfiguration
+    homeConfigurations = {
+    	shiva = home-manager.lib.homeManagerConfiguration {
+	  inherit pkgs;
+	  modules = [ ./home.nix ];
+	};
+    };
+    # */
+
+  };
+}
+
---
+++ home.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "shiva";
+  home.homeDirectory = "/home/shiva";
+
+  home.stateVersion = "23.11"; # DONT CHANGE THIS
+
+  home.packages = [
+    # pkgs.hello
+  ];
+
+  home.file = {
+    "mydir2/test.vim".text = ''
+      this is a test file that is created using home-manager
+    '';
+  };
+
+#also, path to python is ${pkgs.python}/bin/python
+
+  # configuration of my programs
+  programs.git = {
+    enable = true;
+    userName = "shivajreddy";
+    userEmail = "shivajreddy@outlook.com";
+  };
+
+  home.sessionVariables = {
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
