4f487b4b4affc91d92f26b932ac8e7eaaba04c00
shivajreddy <shivajreddy@outlook.com>
2024-03-06 09:35:12 -0500

    updated home

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My Flake";
+  description = "Shiva's NixOS Flake";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
@@ -13,10 +13,10 @@
 
   };
 
-  # outputs = { self, nixpkgs, ... }:
-  # outputs = inputs@{ self, nixpkgs, ... }:
   outputs = inputs@{ self, nixpkgs, home-manager, ... }:
   # outputs = { self, nixpkgs, home-manager, ... }:
+  # outputs = { self, nixpkgs, ... }:
+  # outputs = inputs@{ self, nixpkgs, ... }:
   let 
         lib = nixpkgs.lib; 
         system = "x86_64-linux";
@@ -52,14 +52,6 @@
         inherit system;
         modules = [ 
           ./tars/configuration.nix 
- 	  /* 1.b. Importing home-manager as a module
-          inputs.home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.shiva = import ./home_manager/home.nix;
-          }
-	  # */ 
         ];
       };
     };
@@ -76,7 +68,8 @@
 	  inherit pkgs;
 	  # modules = [ /home/shiva/dotfiles/home/home.nix ];
 	  #modules = [ "${self}./home/home.nix" ]; # this is an absoulte path apparently
-	  modules = [ "./home/home.nix" ];
+	  modules = [ ( ./. + "/home/default.nix" ) ];
+	  # imports = [ ./home/default.nix ];
 	};
     };
     # */
