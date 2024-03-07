5d23253655b5f2992d41aa6e2e0ca3b16899ae75
shivajreddy <shivajreddy@outlook.com>
2024-03-07 10:41:46 -0500

    updating starship

--- home/apps/starship/starship.nix
+++ home/apps/starship/starship.nix
@@ -1,7 +1,8 @@
 { config, lib, pkgs, ... }:
 
 let 
-  tomlFile = builtins.readFile (./. + "/starship/starship.toml");
+  # tomlFile = builtins.readFile (./. + "/starship/starship.toml");
+  tomlFile = builtins.readFile (./. + "/starship.toml");
 in {
 	imports = [];
 
