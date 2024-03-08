ee85c6f53d756fad80795067a632dcad40c4ebda
shivajreddy <shivajreddy@outlook.com>
2024-03-08 09:14:39 -0500

    patched berkeley

--- home/fonts/fonts.nix
+++ home/fonts/fonts.nix
@@ -2,12 +2,15 @@
 {
   imports = [];
 
-  # Fonts
+  /* Fonts
   home.file.".local/share/fonts/BerkeleyMono/BerkeleyMono-Bold.ttf".source = ./berkeley-mono/BerkeleyMono-Bold.ttf;
   home.file.".local/share/fonts/BerkeleyMono/BerkeleyMono-BoldItalic.ttf".source = ./berkeley-mono/BerkeleyMono-BoldItalic.ttf;
   home.file.".local/share/fonts/BerkeleyMono/BerkeleyMono-Italic.ttf".source = ./berkeley-mono/BerkeleyMono-Italic.ttf;
   home.file.".local/share/fonts/BerkeleyMono/BerkeleyMono-Regular.ttf".source = ./berkeley-mono/BerkeleyMono-Regular.ttf;
+  # */
 
-  home.file.".local/share/fonts/sf-san-francisco-pro".source = ./san-francisco-pro;
+  # home.file.".local/share/fonts/sf-san-francisco-pro".source = ./san-francisco-pro;
+  home.file.".local/share/fonts/berkeley-mono".source = ./berkeley-mono;
+  home.file.".local/share/fonts/berkeley-mono-nerdfont".source = ./berkeley-mono-nerdfont;
 
 }
