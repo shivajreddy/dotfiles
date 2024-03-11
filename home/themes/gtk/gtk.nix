614e7a1bb97ffb283461575541df50c78909b3a8
shivajreddy <shivajreddy@outlook.com>
2024-03-11 07:31:32 -0400

    testing catppuccin gtk

--- home/themes/gtk/gtk.nix
+++ home/themes/gtk/gtk.nix
@@ -27,7 +27,7 @@
       # */
 
       # /* Catppuccin Theme -> https://github.com/catppuccin/gtk
-      name = "Mocha-Pink"
+      name = "Mocha-Pink";
       package = pkgs.catppuccin-gtk.override {
         accents = [ "pink" ];
         size = "compact";
@@ -37,13 +37,6 @@
       # */ 
     };
 
-    # /* Catppuccin: Now symlink the `~/.config/gtk-4.0/` folder declaratively:
-    xdg.configFile = {
-      "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
-      "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
-      "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
-    };
-    # */
 
     # Cursors
     cursorTheme = {
@@ -87,5 +80,13 @@
     };
 
   };
+
+  # /* Catppuccin: Now symlink the `~/.config/gtk-4.0/` folder declaratively:
+  xdg.configFile = {
+    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  };
+  # */
 
 }
