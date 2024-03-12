9333844698924f8aa9867f5b45414cca6f4ab640
shivajreddy <shivajreddy@outlook.com>
2024-03-11 23:10:03 -0400

    trying nvim in nix

--- home/default.nix
+++ home/default.nix
@@ -20,7 +20,7 @@
 	(./. + "/apps/zsh.nix")
 	(./. + "/apps/kitty/kitty.nix")
 	(./. + "/apps/tmux/tmux.nix")
-	(./. + "/apps/nvim/nvim.nix")
+	(./. + "/apps/nvim/default.nix")
 	(./. + "/apps/alacritty/alacritty.nix")
 	(./. + "/apps/starship/starship.nix")
 	(./. + "/apps/hyprland/hyprland.nix")
