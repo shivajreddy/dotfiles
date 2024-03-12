e5da3c865d1f822ab226302b9e706865292d6238
shivajreddy <shivajreddy@outlook.com>
2024-03-11 20:19:40 -0400

    added wallpapers

--- home/apps/hyprland/hyprland.nix
+++ home/apps/hyprland/hyprland.nix
@@ -25,30 +25,6 @@ in
 		'';
 	};
 
-
-/* Install pyprland using python3
-    (pkgs.python3Packages.buildPythonPackage rec {
-      pname = "pyprland";
-      version = "1.4.1";
-      src = pkgs.fetchPypi {
-        inherit pname version;
-        sha256 = "sha256-JRxUn4uibkl9tyOe68YuHuJKwtJS//Pmi16el5gL9n8=";
-      };
-      format = "pyproject";
-      propagatedBuildInputs = with pkgs; [
-        python3Packages.setuptools
-        python3Packages.poetry-core
-        poetry
-      ];
-      doCheck = false;
-    })
-  ];
-# */
-
-
- #  home.file.".config/hypr/pyprland.toml".source = ./pyprland.toml;
-
-
   home.file.".config/hypr/pyprland.toml".text = ''
   	  [pyprland]
 	  plugins = ["scratchpads"]
