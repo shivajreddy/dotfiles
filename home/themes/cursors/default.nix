2f768c9f9f16115e44ee0140bb1e5c6af792a2ab
shivajreddy <shivajreddy@outlook.com>
2024-03-13 07:27:07 -0400

    trying custom cursor package

--- home/themes/cursors/default.nix
+++ home/themes/cursors/default.nix
@@ -7,17 +7,28 @@ pkgs.stdenv.mkDerivation {
   name = "bibata-mocha";  # it doesn't matter what you name
 
   src = pkgs.fetchurl {
-    url = "";
-    sah256 = "";
+    url = "https://github.com/shivajreddy/dotfiles/tree/main/home/themes/cursors/bibata-mocha.tar.gz";
+    sha256 = "1r6qc4xw7528g1wzsdk5hmifz19rkjldh80s6pb1lh937ir2chsy";
   };
 
+  dontUnpack = true;
+
+  installPhase = ''
+  echo "Installing Cursor ${name}"
+  mkdir -p $out
+  ${pkgs.unzip}/bin/unzip $src -d $out/
+  '';
+
+}
+
+
+/*
   # Copy the cursor folder
-  # mv Bibata-* ~/.icons/                 # Install to local users
-  # sudo mv Bibata-* /usr/share/icons/    # Install to all users
+  mv Bibata-* ~/.icons/                 # Install to local users
+  sudo mv Bibata-* /usr/share/icons/    # Install to all users
 
   # Copy Bibata-Mocha folder
   home.file."~/.icons/Bibata-Mocha".source = ./Bibata-Mocha;
   home.file."/usr/share/icons/Bibata-Mocha".source = ./Bibata-Mocha;
-  # /home/shiva/.icons
+# */
 
-}
