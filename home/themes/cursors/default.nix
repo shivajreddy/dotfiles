0f5ca7b113d709879909c8c511fe9721dacd92ab
shivajreddy <shivajreddy@outlook.com>
2024-03-13 21:27:45 -0400

    added Bibata custom themes

--- home/themes/cursors/default.nix
+++ home/themes/cursors/default.nix
@@ -1,57 +1,12 @@
-{ pkgs, ... }:
+{ lib, ... }:
+{
+  imports = [];
 
 
-# this is the almighty fn that is used in nix-os to build packages
-pkgs.stdenv.mkDerivation {
+  # home.file.".local/share/fonts/sf-san-francisco-pro".source = ./san-francisco-pro;
+  # home.file.".local/share/fonts/berkeley-mono".source = ./berkeley-mono;
+  # home.file.".local/share/fonts/berkeley-mono-nerdfont".source = ./berkeley-mono-nerdfont;
 
-  name = "bibata-mocha"; # it doesn't matter what you name
+  home.file.".icons/Bibata-Mocha".source = ./Bibata-Mocha;
 
-  src = ./Bibata-Mocha;
-  /*
-  src = pkgs.fetchurl {
-    url = "<has to be a package like stuff with makefile or buildphase>";
-    sha256 = "<its sha256 got by nix-prefetch-url 'above url'>";
-  };
-  # */
-
-  # dontUnpack = true;
-
-  dontWrapGzip = true;
-
-  /*
-  installPhase = ''
-  echo "-------->>>>>>> Installing Cursor ${myName} <<<<<<<---------"
-  echo "-------->>>>>>> src:$src out:$out <<<<<<<---------"
-  mkdir -p $out/share/icons
-  echo "out = $out"
-  cp -r $src/* $out/share/icons/
-  '';
-  # */
-
-  # /*
-  installPhase = ''
-  echo "-------->>>>>>> Installing Cursor <<<<<<<---------"
-  echo "-------->>>>>>> src:$src out:$out <<<<<<<---------"
-  mkdir -p $out/.icons
-  cp -r $src $out/.icons/
-  echo "-------->>>>>>> DONE <<<<<<<---------"
-  '';
-  # */
-  # mkdir -p $out/share/icons
-  # cp -r $src $out/share/icons/
-  # mkdir -p $out
-  # cp -r $src/Bibata-* -d $out
-  # cp -r $src/Bibata-Mocha/ $out/share/icons/Bibata-Mocha
 }
-
-
-/*
-  # Copy the cursor folder
-  mv Bibata-* ~/.icons/                 # Install to local users
-  sudo mv Bibata-* /usr/share/icons/    # Install to all users
-
-  # Copy Bibata-Mocha folder
-  home.file."~/.icons/Bibata-Mocha".source = ./Bibata-Mocha;
-  home.file."/usr/share/icons/Bibata-Mocha".source = ./Bibata-Mocha;
-# */
-
