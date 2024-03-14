9feddb4a32726fee17fe867659a546c58d26051b
shivajreddy <shivajreddy@outlook.com>
2024-03-14 14:12:57 -0400

    .

--- home/themes/cursors/default.nix
+++ home/themes/cursors/default.nix
@@ -4,9 +4,6 @@
 # this is the almighty fn that is used in nix-os to build packages
 pkgs.stdenv.mkDerivation {
 
-  # WARNING: what is the diff b/w name and pname;
-
-  # name = "bibata-mocha"; # it doesn't matter what you name
   pname = "bibata-mocha"; 
   version = "1.0.0";
 
@@ -30,19 +27,8 @@ pkgs.stdenv.mkDerivation {
 }
 
 
-/*
-  # Copy the cursor folder
+/* Copy the cursor folder
   mv Bibata-* ~/.icons/                 # Install to local users
   sudo mv Bibata-* /usr/share/icons/    # Install to all users
-
-  install -dm 0755 $out/share/icons
-  echo "ln -sfn $out/share/icons ~/.icons/bibata-mocha"
-  mkdir -p $out/.icons
-
-  postInstall = ''
-  echo "To link these icons to your home directory, run:"
-  echo "-------->>>>>>> DONE <<<<<<<---------"
-  '';
-
 # */
 
