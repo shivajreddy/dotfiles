09097f35619ec73d070948cee706a82aab40dcf5
shivajreddy <shivajreddy@outlook.com>
2024-03-14 22:00:20 -0400

    .

--- home/apps/nvim/default.nix
+++ home/apps/nvim/default.nix
@@ -138,7 +138,7 @@
 
 
   # Normal LazyVim config here, see https://github.com/LazyVim/starter/tree/main/lua
-  # xdg.configFile."nvim/lua".source = ./lua;
+  xdg.configFile."nvim/lua".source = ./lua;
 
 }
 
