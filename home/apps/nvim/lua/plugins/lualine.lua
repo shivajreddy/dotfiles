941cd1803a49dfa1a610691c7ab9481a0b910803
shivajreddy <shivajreddy@outlook.com>
2024-03-14 22:01:16 -0400

    .

--- home/apps/nvim/lua/plugins/lualine.lua
+++ home/apps/nvim/lua/plugins/lualine.lua
@@ -85,9 +85,9 @@ return {
 					-- { "location", padding = { left = 0, right = 1 } },
 				},
 				lualine_z = {
-					-- function()
-					--   return " " .. os.date("%R")
-					-- end,
+					function()
+						return " " .. os.date("%R")
+					end,
 				},
 			},
 			extensions = { "neo-tree", "lazy" },
