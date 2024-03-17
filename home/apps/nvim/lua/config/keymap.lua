700384cc2fdde840003e8712ea2c204cfd13ab57
shivajreddy <shivajreddy@outlook.com>
2024-03-17 01:23:45 -0400

    .

--- home/apps/nvim/lua/config/keymap.lua
+++ home/apps/nvim/lua/config/keymap.lua
@@ -1,8 +1,16 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+vim.g.mapleader = "<Space>"
 
--- vim.keymap.set("n", "", "", { desc = "" })
+vim.keymap.set("n", "<Leader>/", "gc<CR>", { desc = "Comment Line", silent = true })
 
--- Comment
-vim.keymap.set("n", "<Leader>/", ":gc<CR>", { desc = "Comment Line", silent = true })
+vim.keymap.set("n", "Y", "y$", { desc = "Yank end of line", silent = true })
+vim.keymap.set("n", "<leader>pv", vim.cmd.Explore, { desc = "Open NetRW", silent = true })
+
+vim.keymap.set("n", "<Left>", "<C-w>h<CR>", {desc = "Tab Left"})
+vim.keymap.set("n", "<Down>", "<C-w>j<CR>", {desc = "Tab Down"})
+vim.keymap.set("n", "<Up>", "<C-w>k<CR>", {desc = "Tab Up"})
+vim.keymap.set("n", "<Right>", "<C-w>l<CR>", {desc = "Tab Right"})
+
+-- Not working for somereason
+vim.api.nvim_set_keymap("n", "<Leader>w", ":w<CR>", {desc="Write", silent=true})
+vim.api.nvim_set_keymap("n", "<Leader>wq", ":wq<CR>", {desc="Write & Quit", silent=true})
+
