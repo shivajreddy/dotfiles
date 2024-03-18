8736f0d9c673740bf630e22aac0efc3c9d52984e
shivajreddy <shivajreddy@outlook.com>
2024-03-18 10:49:19 -0400

    .

--- home/apps/tmux/tmux.nix
+++ home/apps/tmux/tmux.nix
@@ -7,13 +7,6 @@
     enable = true;
 
     extraConfig = ''
-      unbind C-b
-      set-option -prefix C-t
-
-      bind -r C-h select-pane -L
-      bind -r C-j select-pane -D
-      bind -r C-k select-pane -U
-      bind -r C-l select-pane -R
     '';
   };
 
