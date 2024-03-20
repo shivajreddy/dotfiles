f22e74271bc1349ea19322713cf876b6d0dab015
shivajreddy <shivajreddy@outlook.com>
2024-03-20 18:27:38 -0400

    .

--- home/apps/nvim/default.nix
+++ home/apps/nvim/default.nix
@@ -113,7 +113,7 @@
             { "williamboman/mason-lspconfig.nvim", enabled = false },
             { "williamboman/mason.nvim", enabled = false },
             -- import/override with your plugins
-            { import = "plugins" },
+            -- { import = "plugins" },
             -- treesitter handled by xdg.configFile."nvim/parser", put this line at the end of spec to clear ensure_installed
             { "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = {} } },
           },
