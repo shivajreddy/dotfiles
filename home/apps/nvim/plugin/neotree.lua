de723b46c9c6faa99df92d1c2fd2b14d95261e0a
shivajreddy <shivajreddy@outlook.com>
2024-03-23 21:05:53 -0400

    .

--- home/apps/nvim/plugin/neotree.lua
+++ home/apps/nvim/plugin/neotree.lua
@@ -6,6 +6,4 @@ require("nvim-neo-tree/neo-tree.nvim").setup({
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     }
-}
-
-
+})
