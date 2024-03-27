cbf57a8153d79b20d6b9427bf7a37a6b2d2add5b
shivajreddy <shivajreddy@outlook.com>
2024-03-27 07:14:24 -0400

    .

--- home/apps/nvim/default.nix
+++ home/apps/nvim/default.nix
@@ -51,6 +51,79 @@ in
     '';
 
     plugins = with pkgs.vimPlugins; [
+    LazyVim
+    better-escape-nvim
+    clangd_extensions-nvim
+    cmp-buffer
+    cmp-nvim-lsp
+    cmp-path
+    cmp_luasnip
+    conform-nvim
+    crates-nvim
+    dracula-nvim
+    dressing-nvim
+    flash-nvim
+    friendly-snippets
+    gitsigns-nvim
+    headlines-nvim
+    indent-blankline-nvim
+    kanagawa-nvim
+    lualine-nvim
+    marks-nvim
+    neo-tree-nvim
+    neoconf-nvim
+    neodev-nvim
+    neorg
+    nix-develop-nvim
+    noice-nvim
+    none-ls-nvim
+    nui-nvim
+    nvim-cmp
+    nvim-dap
+    nvim-dap-ui
+    nvim-dap-virtual-text
+    nvim-lint
+    nvim-lspconfig
+    nvim-notify
+    nvim-spectre
+    nvim-treesitter
+    nvim-treesitter-context
+    nvim-treesitter-textobjects
+    nvim-ts-autotag
+    nvim-ts-context-commentstring
+    nvim-web-devicons
+    oil-nvim
+    overseer-nvim
+    persistence-nvim
+    plenary-nvim
+    project-nvim
+    rust-tools-nvim
+    sqlite-lua
+    telescope-fzf-native-nvim
+    telescope-nvim
+    tmux-navigator
+    todo-comments-nvim
+    tokyonight-nvim
+    trouble-nvim
+    vim-illuminate
+    vim-startuptime
+    vscode-nvim
+    which-key-nvim
+    { name = "LuaSnip"; path = luasnip; }
+    { name = "blame-me.nvim"; path = blame-me-nvim; }
+    { name = "catppuccin"; path = catppuccin-nvim; }
+    { name = "cmake-gtest.nvim"; path = cmake-gtest-nvim; }
+    { name = "cmake-tools.nvim"; path = cmake-tools-nvim; }
+    { name = "huez.nvim"; path = huez-nvim; }
+    { name = "mini.ai"; path = mini-nvim; }
+    { name = "mini.bufremove"; path = mini-nvim; }
+    { name = "mini.comment"; path = mini-nvim; }
+    { name = "mini.indentscope"; path = mini-nvim; }
+    { name = "mini.pairs"; path = mini-nvim; }
+    { name = "mini.surround"; path = mini-nvim; }
+    { name = "yanky.nvim"; path = yanky-nvim; }
+
+    /*
       LazyVim
       neodev-nvim
       telescope-fzf-native-nvim
@@ -98,6 +171,8 @@ in
         ]));
         config = toLuaFile ./plugin/treesitter.lua;
       }
+      */
+
     ];
 
   };
