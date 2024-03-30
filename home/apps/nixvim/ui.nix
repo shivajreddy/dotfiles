529a19ce2d75e7f4343a257b2a02e903382210dc
shivajreddy <shivajreddy@outlook.com>
2024-03-30 10:45:31 -0400

    .

--- home/apps/nixvim/ui.nix
+++ home/apps/nixvim/ui.nix
@@ -1,5 +1,70 @@
 {pkgs, ...}: {
   plugins = {
-    # dressing.enable = true;
+    alpha = {
+      enable = true;
+      # theme = "dashboard";
+      layout = [
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          opts = {
+            hl = "Type";
+            position = "center";
+          };
+          type = "text";
+          val = [
+            "███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗"
+            "████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║"
+            "██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║"
+            "██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║"
+            "██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║"
+            "╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝"
+          ];
+        }
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          type = "group";
+          val = [
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[ene]] end";
+              };
+              opts = {
+                shortcut = "n";
+              };
+              type = "button";
+              val = "  New file";
+            }
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[qa]] end";
+              };
+              opts = {
+                shortcut = "q";
+              };
+              type = "button";
+              val = "󰗼 Quit Neovim";
+            }
+          ];
+        }
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          opts = {
+            hl = "Keyword";
+            position = "center";
+          };
+          type = "text";
+          val = "Inspiring quote here.";
+        }
+      ];
+    };
   };
 }
