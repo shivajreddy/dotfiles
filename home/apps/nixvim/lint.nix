77efd09bf7635536e48b59e4db48c5d9c609db00
shivajreddy <shivajreddy@outlook.com>
2024-03-30 12:10:55 -0400

    .

--- home/apps/nixvim/lint.nix
+++ home/apps/nixvim/lint.nix
@@ -13,7 +13,8 @@
       lua = ["luacheck"];
       markdownlint = ["markdownlint"];
       nix = ["nix"];
-      python = ["ruff"];
+      # python = ["ruff"];
+      python = ["pyright"];
       sh = ["shellcheck"];
       typescript = ["eslint_d"];
       typescriptreact = ["eslint_d"];
