c341aec899dad1cb53ec76bdaf4629a10afa1989
shivajreddy <shivajreddy@outlook.com>
2024-04-01 17:01:43 -0400

    .

--- home/apps/nixvim/ui.nix
+++ home/apps/nixvim/ui.nix
@@ -21,8 +21,18 @@
       timeout = 3000;
       maxHeight = helpers.mkRaw ''
         function()
-        	return math.floor(vim.o.lines * 0.75)
+        return math.floor(vim.o.lines * 0.75)
         end
+      '';
+      maxWidth = helpers.mkRaw ''
+        function()
+        	return math.floor(vim.o.columns * 0.75)
+        end
+      '';
+      onOpen = helpers.mkRaw ''
+        function(win)
+        		vim.api.nvim_win_set_config(win, { zindex = 100 })
+        		end
       '';
     };
 
