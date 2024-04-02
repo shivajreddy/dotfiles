108eed5210f12c48d0b6450ac834087347fe0d65
shivajreddy <shivajreddy@outlook.com>
2024-04-01 20:58:35 -0400

    .

--- home/apps/nixvim/ui.nix
+++ home/apps/nixvim/ui.nix
@@ -39,7 +39,28 @@
     # bufferline
     bufferline = {
       enable = true;
+      closeCommand = helpers.mkRaw ''
+        function(n) require("mini.bufremove").delete(n, false) end,
+      '';
+      rightMouseCommand = helpers.mkRaw ''
+        function(n) require("mini.bufremove").delete(n, false) end,
+      '';
+      diagnostics = "nvim_lsp";
       alwaysShowBufferline = false;
+      diagnosticsIndicator = ''
+        function(_, _, diag)
+           local icons = require("lazyvim.config").icons.diagnostics
+           local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+             .. (diag.warning and icons.Warn .. diag.warning or "")
+           return vim.trim(ret)
+         end
+      '';
+      offsets = {
+        filetype = "neo-tree";
+        text = "Neo-tree";
+        highlight = "Directory";
+        text_align = "left";
+      };
     };
 
     mini = {
