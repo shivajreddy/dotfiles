1f9066317a3a066f39af2bbb180652617e5f7362
shivajreddy <shivajreddy@outlook.com>
2024-04-02 07:01:34 -0400

    .

--- home/apps/nixvim/ui.nix
+++ home/apps/nixvim/ui.nix
@@ -79,7 +79,7 @@
       sections = {
         lualine_a = ["mode"];
         lualine_b = ["branch"];
-        lualine_c = ["branch" "function() return LazyVim.lualine.root_dir() end"];
+        lualine_c = ["branch"];
       };
     };
 
