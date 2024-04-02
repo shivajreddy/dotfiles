58db6eec5fe57639261180479d5ca113b5f4970e
shivajreddy <shivajreddy@outlook.com>
2024-04-02 18:22:02 -0400

    .

--- home/apps/nixvim/ui.nix
+++ home/apps/nixvim/ui.nix
@@ -92,7 +92,7 @@
           {
             name = "filetype";
             icons_enabled = true;
-            fmt = "";
+            fmt = null;
             separator = {left = "";};
             padding = {
               left = 1;
