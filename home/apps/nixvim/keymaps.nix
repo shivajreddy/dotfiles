f7ae3195487d37bbd90445d91fd3100f61bd67b5
shivajreddy <shivajreddy@outlook.com>
2024-04-03 11:20:03 -0400

    .

--- home/apps/nixvim/keymaps.nix
+++ home/apps/nixvim/keymaps.nix
@@ -249,13 +249,26 @@
     }
 
     # flash
+    /*
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    */
     {
       mode = ["n" "x" "o"];
+      key = "s";
+      action = helpers.mkRaw ''
+        function() require("flash").jump() end
+      '';
+      options.desc = "Flas";
     }
 
+    # # # # # # EXISTING
     {
-      key = "<leader>u";
       mode = "n";
+      key = "<leader>u";
       action = "<cmd>UndotreeToggle<CR>";
       options = {
         silent = true;
