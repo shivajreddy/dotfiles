17b925f39b823f4c4ec8a5a43044e73a0d666c63
shivajreddy <shivajreddy@outlook.com>
2024-04-03 12:33:23 -0400

    .

--- home/apps/nixvim/editor.nix
+++ home/apps/nixvim/editor.nix
@@ -28,10 +28,6 @@
       {
            keys = helpers.mkRaw ''
                  {
-              { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
-             { "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
-             { "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
-             { "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
              {
              	"[q",
              	function()
@@ -62,7 +58,7 @@
              },
              }
            '';
-         };
+      };
       */
     };
 
