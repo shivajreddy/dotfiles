a4b8e4c9e5ea9af48c5e5069f4ecba522454be16
shivajreddy <shivajreddy@outlook.com>
2024-04-03 19:04:57 -0400

    .

--- home/apps/nixvim/keymaps.nix
+++ home/apps/nixvim/keymaps.nix
@@ -14,5 +14,15 @@
         desc = "Prev quickfix location";
       };
     }
+    {
+      mode = "n";
+      key = "<C-n>";
+      action = "<cmd>Neotree toggle<CR>";
+      options = {
+        silent = true;
+        noremap = true;
+        desc = "Toggle Neotree tree";
+      };
+    }
   ];
 }
