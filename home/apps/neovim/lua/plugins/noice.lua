cb5438ddf59e2821e6dc56ebfcb2dfeab39200ec
shivajreddy <shivajreddy@outlook.com>
2024-04-04 16:40:31 -0400

    .

--- home/apps/neovim/lua/plugins/noice.lua
+++ home/apps/neovim/lua/plugins/noice.lua
@@ -1,16 +1,4 @@
 return {
-	--[[
-	-- override nvim-cmp and add cmp-emoji
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = { "hrsh7th/cmp-emoji" },
-		---@param opts cmp.ConfigSchema
-		opts = function(_, opts)
-			table.insert(opts.sources, { name = "emoji" })
-		end,
-	},
-  --]]
-	--
 	-- lazy.nvim
 	{
 		"folke/noice.nvim",
