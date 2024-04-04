af387d49a0ef05721bfa8b212409eb8b81b0d791
shivajreddy <shivajreddy@outlook.com>
2024-04-04 12:01:25 -0400

    .

--- home/apps/nixvim_fuck/default.nix
+++ home/apps/nixvim_fuck/default.nix
@@ -95,7 +95,7 @@
 
     # /*
     extraConfigLua = ''
-        -- require("neoconf").setup({})
+        require("neoconf").setup({})
         require("lazy").setup({
           defaults = {
             lazy = true,
