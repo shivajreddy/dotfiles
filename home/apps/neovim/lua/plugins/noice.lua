ac26169788eb7ed01f90d7f9287f24aa48ddbcfe
shivajreddy <shivajreddy@outlook.com>
2024-04-04 18:02:46 -0400

    .

--- home/apps/neovim/lua/plugins/noice.lua
+++ home/apps/neovim/lua/plugins/noice.lua
@@ -7,5 +7,61 @@ return {
 				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 			},
 		},
+
+		format = {
+			cmdline = { pattern = "^:", icon = "", lang = "vim" },
+			search_down = { kind = "search", pattern = "^/", icon = "🔍", lang = "regex" },
+			search_up = { kind = "search", pattern = "^%?", icon = "🔍", lang = "regex" },
+			filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+			lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+			help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+			input = {}, -- Used by input()
+		},
 	}),
 }
+
+--[[
+  plugins = {
+    noice = {
+      enable = true;
+      presets = {
+        bottom_search = true;
+      };
+      cmdline.format = {
+        cmdline = {icon = ">";};
+        search_down = {icon = "🔍⌄";};
+        search_up = {icon = "🔍⌃";};
+        filter = {icon = "$";};
+        lua = {icon = "☾";};
+        help = {icon = "?";};
+      };
+      format = {
+        level = {
+          icons = {
+            error = "✖";
+            warn = "▼";
+            info = "●";
+          };
+        };
+      };
+      popupmenu = {
+        kindIcons = false;
+      };
+      extraOptions = {
+        inc_rename.cmdline.format.IncRename = {icon = "⟳";};
+      };
+    };
+  };
+  extraConfigLua = ''
+    -- Noice recommended config
+    require("noice").setup({
+    		lsp = {
+    		override = {
+    		["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+    		["vim.lsp.util.stylize_markdown"] = true,
+    		["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    		},
+    		},
+    		})
+  '';
+-]]
