388670c1d779b005bd53bf7d1f57d3cfe460448c
shivajreddy <shivajreddy@outlook.com>
2024-04-03 21:14:56 -0400

    .

--- flake.nix
+++ flake.nix
@@ -12,11 +12,14 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # NIXVIM
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    /*
+         NIXVIM
+      nixvim = {
+        url = "github:nix-community/nixvim";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+    #
+    */
 
     # Other Flakes...
     # /* Testing onedark theme flake
@@ -25,47 +28,48 @@
       flake = false;
     };
     # */
-
-
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      lib = nixpkgs.lib;
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
       system = "x86_64-linux";
-      pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
-    in
-    {
-      nixosConfigurations = {
-        predator = lib.nixosSystem {
-          # extraSpecialArgs = { inherit inputs; };
-          inherit system;
-          modules = [
-            (./. + "/hosts/predator/configuration.nix")
-          ];
-        };
-        tars = lib.nixosSystem {
-          # extraSpecialArgs = { inherit inputs; };
-          inherit system;
-          modules = [
-            (./. + "/hosts/tars/configuration.nix")
-          ];
-        };
-      };
-
-      # HomeConfiguration -- using as a package
-      homeConfigurations = {
-        shiva = inputs.home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [ 
-	  	(./. + "/home/default.nix") 
-		inputs.nixvim.homeManagerModules.nixvim
-	  ];
-
-          extraSpecialArgs = { inherit inputs; };
-        };
-      };
-
+      config.allowUnfree = true;
     };
-}
+  in {
+    nixosConfigurations = {
+      predator = lib.nixosSystem {
+        # extraSpecialArgs = { inherit inputs; };
+        inherit system;
+        modules = [
+          (./. + "/hosts/predator/configuration.nix")
+        ];
+      };
+      tars = lib.nixosSystem {
+        # extraSpecialArgs = { inherit inputs; };
+        inherit system;
+        modules = [
+          (./. + "/hosts/tars/configuration.nix")
+        ];
+      };
+    };
 
+    # HomeConfiguration -- using as a package
+    homeConfigurations = {
+      shiva = inputs.home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          (./. + "/home/default.nix")
+          # inputs.nixvim.homeManagerModules.nixvim
+        ];
+
+        extraSpecialArgs = {inherit inputs;};
+      };
+    };
+  };
+}
