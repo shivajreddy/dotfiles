87449cd3e583e793fa8ac2c958d2dac4f4d0e8c0
shivajreddy <shivajreddy@outlook.com>
2024-04-04 18:07:48 -0400

    .

--- home/apps/neovim/lua/plugins/noice.lua
+++ home/apps/neovim/lua/plugins/noice.lua
@@ -12,8 +12,8 @@ return {
 
 		format = {
 			cmdline = { pattern = "^:", icon = "", lang = "vim" },
-			search_down = { kind = "search", pattern = "^/", icon = "🔍", lang = "regex" },
-			search_up = { kind = "search", pattern = "^%?", icon = "🔍", lang = "regex" },
+			search_down = { kind = "search", pattern = "^/", icon = " 🔍 ", lang = "regex" },
+			search_up = { kind = "search", pattern = "^%?", icon = " 🔍 ", lang = "regex" },
 			filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
 			lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
 			help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
