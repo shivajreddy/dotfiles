c758818218856a2c781dfb660e1052d57734b7d6
shivajreddy <shivajreddy@outlook.com>
2024-04-04 16:45:39 -0400

    .

--- home/apps/neovim/lua/plugins/noice.lua
+++ home/apps/neovim/lua/plugins/noice.lua
@@ -1,3 +1,4 @@
+--[[
 return {
 	-- lazy.nvim
 	{
@@ -14,4 +15,16 @@ return {
 			},
 		},
 	},
+}
+--]]
+return {
+	require("noice").setup({
+		lsp = {
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+			},
+		},
+	}),
 }
