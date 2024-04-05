ebfea3fcbd4d8121fb7c9585045e49efb27ef3fd
shivajreddy <shivajreddy@outlook.com>
2024-04-05 10:35:26 -0400

    .

--- home/apps/neovim/lua/plugins/noice.lua
+++ home/apps/neovim/lua/plugins/noice.lua
@@ -12,6 +12,19 @@ return {
 
 		presets = { bottom_search = true },
 
+		views = {
+			cmdline_popup = {
+				border = {
+					style = "none",
+					padding = { 2, 3 },
+				},
+				filter_options = {},
+				win_options = {
+					winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+				},
+			},
+		},
+
 		popupmenu = {
 			enabled = true,
 			backend = "nui",
