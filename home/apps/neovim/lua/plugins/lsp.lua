ab6e2ad33a721579889c85418b1b8913a2bf37f3
shivajreddy <shivajreddy@outlook.com>
2024-04-05 10:03:23 -0400

    .

--- home/apps/neovim/lua/plugins/lsp.lua
+++ home/apps/neovim/lua/plugins/lsp.lua
@@ -4,7 +4,6 @@ return {
 		"neovim/nvim-lspconfig",
 		---@class PluginLspOpts
 		opts = {
-			---@type lspconfig.options
 			servers = {
 				-- pyright will be automatically installed with mason and loaded with lspconfig
 				pyright = {},
