9912fb4832b9520bf70328b9cea5fa3912cf50b1
shivajreddy <shivajreddy@outlook.com>
2024-04-05 08:39:19 -0400

    .

--- home/apps/neovim/lua/plugins/cmp.lua
+++ home/apps/neovim/lua/plugins/cmp.lua
@@ -1,39 +1,32 @@
-return {
-	{ "hrsh7th/cmp-cmdline" },
-	{
-		"hrsh7th/nvim-cmp",
+local M = {}
 
-		config = function()
-			local cmp = require("cmp")
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{
-						name = "cmdline",
-						option = {
-							ignore_cmds = { "Man", "!" },
-						},
-					},
-				}, {
-					{ name = "path" },
-				}),
-			})
+M.config = function()
+	local cmp = require("cmp")
 
-			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = "buffer" },
-				},
-			})
+	cmp.setup({
+		snippet = {
+			expand = function(args)
+				require("luasnip").lsp_expand(args.body)
+			end,
+		},
+		mapping = {
+			["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+			["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+			["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "s" }),
+			["<C-e>"] = cmp.mapping({
+				i = cmp.mapping.abort(),
+				c = cmp.mapping.close(),
+			}),
+			["<CR>"] = cmp.mapping.confirm({ select = true }),
+		},
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp" },
+			{ name = "luasnip" },
+			-- more sources can be added here
+		}, {
+			{ name = "buffer" },
+		}),
+	})
+end
 
-			cmp.setup({
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "luasnip", keyword_length = 2 },
-					{ name = "buffer", keyword_length = 3 },
-					{ name = "path" },
-				},
-			})
-		end,
-	},
-}
+return M
