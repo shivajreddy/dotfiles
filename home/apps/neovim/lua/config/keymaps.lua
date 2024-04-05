b861e30ad234c8ace980447b6e30a546a04c40e8
shivajreddy <shivajreddy@outlook.com>
2024-04-05 12:05:02 -0400

    .

--- home/apps/neovim/lua/config/keymaps.lua
+++ home/apps/neovim/lua/config/keymaps.lua
@@ -3,18 +3,3 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<C-n>", "<cmd>Neotree toggle<CR>", { desc = "Neotree Toggle" })
-
--- Obsidian
-vim.keymap.set(
-	"n",
-	"<leader>oc",
-	"<cmd>lua require('obsidian').util.toggle_checkbox()<CR>",
-	{ desc = "Obsidian Check Checkbox" }
-)
-vim.keymap.set("n", "<leader>ot", "<cmd>ObsidianTemplate<CR>", { desc = "Insert Obsidian Template" })
-vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianOpen<CR>", { desc = "Open in Obsidian App" })
-vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { desc = "Show ObsidianBacklinks" })
-vim.keymap.set("n", "<leader>ol", "<cmd>ObsidianLinks<CR>", { desc = "Show ObsidianLinks" })
-vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>", { desc = "Create New Note" })
-vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<CR>", { desc = "Search Obsidian" })
-vim.keymap.set("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Quick Switch" })
