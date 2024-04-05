370e96bfd8bc1faf50c8f2444ebfec78576f6c4f
shivajreddy <shivajreddy@outlook.com>
2024-04-05 08:19:54 -0400

    .

--- home/apps/neovim/lua/plugins/cmp.lua
+++ home/apps/neovim/lua/plugins/cmp.lua
@@ -25,6 +25,15 @@ return {
 					{ name = "buffer" },
 				},
 			})
+
+			cmp.setup({
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip", keyword_length = 2 },
+					{ name = "buffer", keyword_length = 3 },
+					{ name = "path" },
+				},
+			})
 		end,
 	},
 }
