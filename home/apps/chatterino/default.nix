3b17dcd7adc700fd72669fde8d5257b0270e201b
shivajreddy <shivajreddy@outlook.com>
2024-04-07 10:49:33 -0400

    .

--- home/apps/chatterino/default.nix
+++ home/apps/chatterino/default.nix
@@ -6,6 +6,7 @@
 
   # xdg.configFile."swaync".source = ../swaync/swaync_config;
   # copy the settings folder into .local/share/chatterino2
+  # home.file.".local/share/fonts/berkeley-mono-nerdfont".source = ./berkeley-mono-nerdfont;
   home.file.".local/share/catterino2/Settings".source = ./config/Settings;
 
   # copy the themes folder into .local/share/chatterino2
