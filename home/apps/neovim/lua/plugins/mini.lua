62fec74ec2c0f1818664ce57be56f101ce1f3d5f
shivajreddy <shivajreddy@outlook.com>
2024-04-07 12:01:38 -0400

    .

---
+++ home/apps/neovim/lua/plugins/mini.lua
@@ -0,0 +1,11 @@
+return {
+	"echasnovski/mini.nvim",
+	opts = {
+		mappings = {
+			comment = "gc",
+			comment_line = "gcc",
+			-- Toggle comment on visual selection
+			comment_visual = "gc",
+		},
+	},
+}
