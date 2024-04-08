19715ab364e9b2694f261bd1e4233b5bb5370b1a
shivajreddy <shivajreddy@outlook.com>
2024-04-08 08:48:57 -0400

    .

--- home/apps/neovim/lua/plugins/autosave.lua
+++ home/apps/neovim/lua/plugins/autosave.lua
@@ -48,8 +48,8 @@ return {
 			end,
 			dim = 0.18, -- dim the color of `message`
 			cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-			--]]
 		},
+		--]]
 		trigger_events = { -- See :h events
 			immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
 			defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
