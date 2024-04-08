95f28a57307cfb13eb8a03e7ad9fd5c5080b8782
shivajreddy <shivajreddy@outlook.com>
2024-04-07 23:06:33 -0400

    .

--- home/apps/neovim/lua/plugins/lualine.lua
+++ home/apps/neovim/lua/plugins/lualine.lua
@@ -89,16 +89,16 @@ return {
 					},
 				},
 				lualine_y = {
-					{ "progress", separator = " ", padding = { left = 1, right = 0 } },
+					-- { "progress", separator = " ", padding = { left = 1, right = 0 } },
 					{ "location", padding = { left = 0, right = 1 } },
 				},
-				--[[
 				lualine_z = {
+					--[[
 					function()
 						return " " .. os.date("%R")
 					end,
-				},
         --]]
+				},
 			},
 			extensions = { "neo-tree", "lazy" },
 		}
