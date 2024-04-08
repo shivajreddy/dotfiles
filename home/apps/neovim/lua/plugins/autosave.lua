7208cd82877a67ce3b56249f8c19755b39d9f7bc
shivajreddy <shivajreddy@outlook.com>
2024-04-08 08:45:38 -0400

    .

--- home/apps/neovim/lua/plugins/autosave.lua
+++ home/apps/neovim/lua/plugins/autosave.lua
@@ -42,11 +42,13 @@ return {
 		enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
 		execution_message = {
 			enabled = false,
+			--[[
 			message = function() -- message to print on save
 				return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
 			end,
 			dim = 0.18, -- dim the color of `message`
 			cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+      --]]
 		},
 		trigger_events = { -- See :h events
 			immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
