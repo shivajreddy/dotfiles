f3c4a7b30764cc3fd589c457e4a8407c4c67e9f4
shivajreddy <shivajreddy@outlook.com>
2024-04-08 08:44:14 -0400

    .

--- home/apps/neovim/lua/plugins/autosave.lua
+++ home/apps/neovim/lua/plugins/autosave.lua
@@ -39,16 +39,12 @@ return {
 	cmd = "ASToggle", -- optional for lazy loading on command
 	event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
 	opts = {
-		-- your config goes here
-		-- or just leave it empty :)
 		enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
 		execution_message = {
-			enabled = true,
-			--[[
+			enabled = false,
 			message = function() -- message to print on save
 				return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
 			end,
-      --]]
 			dim = 0.18, -- dim the color of `message`
 			cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
 		},
