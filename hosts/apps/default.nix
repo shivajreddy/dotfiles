e34556ed9856d8f4594d9a505e4b039c64da20ae
shivajreddy <shivajreddy@outlook.com>
2024-04-10 09:40:54 -0400

    .

--- hosts/apps/default.nix
+++ hosts/apps/default.nix
@@ -2,32 +2,30 @@
 # This file is for listing all system apps to be installed
 
 { pkgs }:
-{
-  imports = [
-    ../apps/pcloud
-    ../apps/discord
-  ];
+let 
+  myPcloud = import ../apps/pcloud
+  myDiscord = import ../apps/discord
+in
 
-  # System packages
-  environment.systemPackages = with pkgs; [
-    vim
-    zsh 
-    git
-    gvfs
-    vlc
-    streamlink
-    streamlink-twitch-gui-bin
-    chatterino2 
-    pcloud
-    gnome.gnome-tweaks
-    gnome.nautilus
-    mojave-gtk-theme
-    catppuccin-cursors.mochaDark
+# System packages
+with pkgs; [
+  myPcloud
+  myDiscord
+  vim
+  zsh
+  git
+  gvfs
+  vlc
+  streamlink
+  streamlink-twitch-gui-bin
+  chatterino2
+  # pcloud
+  gnome.gnome-tweaks
+  gnome.nautilus
+  mojave-gtk-theme
+  catppuccin-cursors.mochaDark
 
-    home-manager
-    openssh
-  ];
-}
-
-
+  home-manager
+  openssh
+];
 
