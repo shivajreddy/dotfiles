77ca2eb9eb928668d59de268fcd33cacfa11d4b9
shivajreddy <shivajreddy@outlook.com>
2024-04-12 09:28:23 -0400

    .

--- home/apps/neovim/default.nix
+++ home/apps/neovim/default.nix
@@ -107,7 +107,6 @@
           { name = "mini.surround"; path = mini-nvim; }
 
           obsidian-nvim
-          nvim-dap
         ];
         mkEntryFromDrv = drv:
           if lib.isDerivation drv then
