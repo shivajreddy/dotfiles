5120d91581575e4f8f3d47fbe919c49c83a0ff98
shivajreddy <shivajreddy@outlook.com>
2024-04-13 17:41:31 -0400

    .

--- home/apps/chatterino/default.nix
+++ home/apps/chatterino/default.nix
@@ -1,16 +1,10 @@
-{...}:
+{}:
 
 {
-
-	# programs.chatterino2.enable = true;
-
-  # xdg.configFile."swaync".source = ../swaync/swaync_config;
-  # copy the settings folder into .local/share/chatterino2
-  # home.file.".local/share/fonts/berkeley-mono-nerdfont".source = ./berkeley-mono-nerdfont;
+  # Note1: copy the settings folder into .local/share/chatterino2
   home.file.".local/share/catterino2/Settings".source = ./config/Settings;
 
-  # copy the themes folder into .local/share/chatterino2
+  # Note2: copy the themes folder into .local/share/chatterino2
   home.file.".local/share/catterino2/Themes".source = ./config/Themes;
-
 }
 
