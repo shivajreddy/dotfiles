8b95a25da7f2de1d0cd2a02600957453ccfc6d3c
shivajreddy <shivajreddy@outlook.com>
2024-04-14 13:37:57 -0400

    .

--- home/apps/chatterino/default.nix
+++ home/apps/chatterino/default.nix
@@ -1,6 +1,6 @@
 {
   # Note1: copy the settings folder into .local/share/chatterino2
-  # home.file.".local/share/chatterino/Settings".source = ./config/Settings;
+  home.file.".local/share/chatterino/Settings".source = ./config/Settings;
 
   # Note2: copy the themes folder into .local/share/chatterino2
   home.file.".local/share/chatterino/Themes".source = ./config/Themes;
