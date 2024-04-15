d3df31262adadd532030e6df437a05622fc8ecd8
shivajreddy <shivajreddy@outlook.com>
2024-04-15 07:28:02 -0400

    .

--- home/apps/waybar/default.nix
+++ home/apps/waybar/default.nix
@@ -7,8 +7,9 @@
     enable = true;
   };
 
-  
+
   xdg.configFile."waybar".source = ./waybar_config;
+
   # save swaynotificationcenter config
   xdg.configFile."swaync".source = ../swaync/swaync_config;
 
