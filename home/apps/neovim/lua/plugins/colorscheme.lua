48fb53a741f758f6f875923df0d859bcf8155119
shivajreddy <shivajreddy@outlook.com>
2024-04-16 16:48:23 -0400

    .

--- home/apps/neovim/lua/plugins/colorscheme.lua
+++ home/apps/neovim/lua/plugins/colorscheme.lua
@@ -168,8 +168,8 @@ return {
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "catppuccin",
-			-- colorscheme = "rose-pine-main", -- rose-pine-moon  rose-pine-dawn
+			-- colorscheme = "catppuccin",
+			colorscheme = "rose-pine-main", -- rose-pine-moon  rose-pine-dawn
 			-- colorscheme = "tokyonight",
 		},
 	},
