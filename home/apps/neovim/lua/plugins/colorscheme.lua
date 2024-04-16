e4369208fadfb01c5822a980e0e892137583bbf7
shivajreddy <shivajreddy@outlook.com>
2024-04-16 07:01:51 -0400

    .

--- home/apps/neovim/lua/plugins/colorscheme.lua
+++ home/apps/neovim/lua/plugins/colorscheme.lua
@@ -14,6 +14,41 @@ return {
 		lazy = true,
 		name = "catppuccin",
 		opts = {
+
+			flavour = "auto", -- latte, frappe, macchiato, mocha
+			background = { -- :h background
+				light = "latte",
+				dark = "mocha",
+			},
+			transparent_background = true, -- disables setting the background color.
+			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+			dim_inactive = {
+				enabled = false, -- dims the background color of inactive window
+				shade = "dark",
+				percentage = 0.15, -- percentage of the shade to apply to the inactive window
+			},
+			no_italic = false, -- Force no italic
+			no_bold = false, -- Force no bold
+			no_underline = false, -- Force no underline
+			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+				comments = { "italic" }, -- Change the style of comments
+				conditionals = { "italic" },
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+				operators = {},
+				-- miscs = {}, -- Uncomment to turn off hard-coded styles
+			},
+			color_overrides = {},
+			custom_highlights = {},
+			default_integrations = true,
 			integrations = {
 				aerial = true,
 				alpha = true,
@@ -28,7 +63,10 @@ return {
 				lsp_trouble = true,
 				mason = true,
 				markdown = true,
-				mini = true,
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
 				native_lsp = {
 					enabled = true,
 					underlines = {
@@ -43,6 +81,7 @@ return {
 				neotree = true,
 				noice = true,
 				notify = true,
+				nvimtree = true,
 				semantic_tokens = true,
 				telescope = true,
 				treesitter = true,
