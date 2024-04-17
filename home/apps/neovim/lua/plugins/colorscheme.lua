1bde85e461af74a6045022b7097f835bce8dd594
shivajreddy <shivajreddy@outlook.com>
2024-04-17 06:05:04 -0400

    .

--- home/apps/neovim/lua/plugins/colorscheme.lua
+++ home/apps/neovim/lua/plugins/colorscheme.lua
@@ -46,7 +46,11 @@ return {
 				operators = {},
 				-- miscs = {}, -- Uncomment to turn off hard-coded styles
 			},
-			color_overrides = {},
+			color_overrides = {
+				mocha = {
+					base = "#11111b",
+				},
+			},
 			custom_highlights = {},
 			default_integrations = true,
 			integrations = {
