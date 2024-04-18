81338562f8031137713bc524f35593e153854824
shivajreddy <shivajreddy@outlook.com>
2024-04-18 11:46:55 -0400

    .

--- home/apps/neovim/lua/plugins/bufferline.lua
+++ home/apps/neovim/lua/plugins/bufferline.lua
@@ -6,13 +6,15 @@ return {
 			show_buffer_close_icons = false,
 		},
 
+		highlights = require("catppuccin.groups.integrations.bufferline").get(),
+
 		-- Customize Catppuccin colors
 		-- https://github.com/catppuccin/nvim?tab=readme-ov-file
-		after = "catppuccin",
-		config = function()
-			require("bufferline").setup({
-				highlights = require("catppuccin.groups.integrations.bufferline").get(),
-			})
-		end,
+		-- after = "catppuccin",
+		-- config = function()
+		-- 	require("bufferline").setup({
+		-- 		highlights = require("catppuccin.groups.integrations.bufferline").get(),
+		-- 	})
+		-- end,
 	},
 }
