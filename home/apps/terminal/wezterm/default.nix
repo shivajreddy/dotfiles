239adb846b9bade560cb12fa9fdbc2e952272425
shivajreddy <shivajreddy@outlook.com>
2024-04-18 18:09:40 -0400

    .

--- home/apps/terminal/wezterm/default.nix
+++ home/apps/terminal/wezterm/default.nix
@@ -7,5 +7,39 @@
 
   # Copy the theme folder
   # xdg.configFile."kitty/themes".source = ./themes;
+
+  programs.wezterm.extraConfig = ''
+    -- Pull in the wezterm API
+
+    local wezterm = require("wezterm")
+
+    -- This will hold the configuration.
+
+    local config = wezterm.config_builder()
+
+    config.default_prog = { "tmux" }
+
+    -- This is where you actually apply your config choices
+
+    -- For example, changing the color scheme:
+
+    config.color_scheme = "rose-pine"
+
+    config.font = wezterm.font("IBM Plex Mono")
+    config.font_size = 14
+
+    config.use_fancy_tab_bar = false
+    config.enable_tab_bar = false
+
+    config.window_padding = {
+      top = 0,
+      right = 0,
+      bottom = 0,
+      left = 0,
+    }
+
+    -- and finally, return the configuration to wezterm
+    return config
+  '';
 }
 
