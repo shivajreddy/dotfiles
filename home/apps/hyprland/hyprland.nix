fb84f914ae0741f3b914f150f1fa9fd209e5f996
shivajreddy <shivajreddy@outlook.com>
2024-04-18 20:21:34 -0400

    .

--- home/apps/hyprland/hyprland.nix
+++ home/apps/hyprland/hyprland.nix
@@ -31,7 +31,7 @@ in
 	  plugins = ["scratchpads"]
 
 	  [scratchpads.term]
-	  command = "wezterm start --class 'scratchpad'"
+	  command = "wezterm --config-file ~/dotfiles/home/apps/terminal/wezterm/scratchpad_config.lua start --class scratchpad"
 	  position = "25% 25%"
 	  size = "50% 50%"
 	  class = "scratchpad"
