2886b8e94ee11c50e314433519a2b0972c11de6e
shivajreddy <shivajreddy@outlook.com>
2024-04-19 10:11:04 -0400

    .

--- home/apps/terminal/wezterm/main_config.lua
+++ home/apps/terminal/wezterm/main_config.lua
@@ -9,21 +9,109 @@ config.enable_wayland = false
 
 config.audible_bell = "Disabled"
 
-config.default_prog = { "tmux" }
+-- config.default_prog = { "tmux" }
 
 config.color_scheme = "rose-pine"
 
 config.font = wezterm.font("IBM Plex Mono")
 config.font_size = 14
 
-config.use_fancy_tab_bar = false
-config.enable_tab_bar = false
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = true
 
 config.window_padding = {
 	top = 0,
 	right = 0,
 	bottom = 0,
 	left = 0,
+}
+
+-- Start :
+local function is_vim(pane)
+	-- this is set by the plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+local direction_keys = {
+	Left = "h",
+	Down = "j",
+	Up = "k",
+	Right = "l",
+	-- reverse lookup
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+-- End
+
+-- TMUX TO WEZTERM Leader is the same as my old tmux prefix
+config.leader = { key = "t", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	-- splitting
+	{
+		mods = "LEADER",
+		key = "h",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "v",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+
+	-- Tabs
+	{
+		mods = "LEADER",
+		key = "c",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		mods = "LEADER",
+		key = "x",
+		action = wezterm.action.CloseCurrentTab({ confirm = true }),
+	},
+
+	-- Zooming a Pane
+	{
+		mods = "LEADER",
+		key = "z",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+
+	-- activate copy mode or vim mode
+	{
+		key = "Enter",
+		mods = "LEADER",
+		action = wezterm.action.ActivateCopyMode,
+	},
+
+	-- move between split panes
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
 }
 
 return config
