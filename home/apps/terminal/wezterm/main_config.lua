6acb7090710c68edfbf3e7f3e13cb1ce79430ae3
shivajreddy <shivajreddy@outlook.com>
2024-04-19 06:20:19 -0400

    .

--- home/apps/terminal/wezterm/main_config.lua
+++ home/apps/terminal/wezterm/main_config.lua
@@ -1,3 +1,6 @@
+-- This is the main configuration of my wezterm
+-- For instant testing, create a file .wezter.lua at ~ so that it loads before this
+-- file which would be in the .config
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
@@ -13,6 +16,7 @@ config.color_scheme = "rose-pine"
 config.font = wezterm.font("IBM Plex Mono")
 config.font_size = 14
 
+# 
 config.use_fancy_tab_bar = false
 config.enable_tab_bar = false
 
