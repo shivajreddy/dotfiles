a3b2fe1e75f2505a5f10708755128f3a173fa654
shivajreddy <shivajreddy@outlook.com>
2024-04-19 09:36:14 -0400

    .

--- home/apps/neovim/lua/config/keymaps.lua
+++ home/apps/neovim/lua/config/keymaps.lua
@@ -5,3 +5,9 @@
 vim.keymap.set("n", "<C-n>", "<cmd>Neotree toggle<CR>", { desc = "Neotree Toggle" })
 
 vim.api.nvim_set_keymap("n", "<leader>as", ":ASToggle<CR>", {})
+
+vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
