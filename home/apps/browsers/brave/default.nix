4f71dc9d37ab454eba42cd446a51108dd412757a
shivajreddy <shivajreddy@outlook.com>
2024-04-21 11:39:53 -0400

    .

--- home/apps/browsers/brave/default.nix
+++ home/apps/browsers/brave/default.nix
@@ -1,7 +1,5 @@
 {config, pkgs, ...}:
 {
-
   programs.brave.enable = true;
-
 }
 
--- home/apps/browsers/chrome/default.nix
+++ home/apps/browsers/chrome/default.nix
@@ -1,6 +1,22 @@
 {config, pkgs, ...}:
-{
 
+let
+    chromeDesktopFile = pkgs.writeTextFile {
+    name = "chrome.desktop";
+    destination = "/share/applications/chrome.desktop";
+    text = ''
+      [Desktop Entry]
+      Type=Application
+      Name=google Chrome
+      Exec=google-chrome-stable --ozone-platform=wayland
+      Icon=chrome
+    '';
+  };
+in 
+{
   programs.google-chrome.enable = true;
+
+  # Create .desktop file for rofi to run with args
+  home.file.".local/share/applications/chrome.desktop".source = chromeDesktopFile;
 
 }
