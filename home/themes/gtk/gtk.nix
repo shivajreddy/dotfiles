32c0b8620bd2703244415829c5e67071d4fa10d2
shivajreddy <shivajreddy@outlook.com>
2024-04-22 12:32:24 -0400

    .

--- home/themes/gtk/gtk.nix
+++ home/themes/gtk/gtk.nix
@@ -70,13 +70,13 @@ in
 
 
   # useful link: https://github.com/catppuccin/gtk/issues/110
-  home.file.".config/gtk-4.0/gtk.css".source = "${my_catppuccin}/share/themes/${my_catppuccin_name}/gtk-4.0/gtk.css";
+  # home.file.".config/gtk-4.0/gtk.css".source = "${my_catppuccin}/share/themes/${my_catppuccin_name}/gtk-4.0/gtk.css";
   home.file.".config/gtk-4.0/gtk-dark.css".source = "${my_catppuccin}/share/themes/${my_catppuccin_name}/gtk-4.0/gtk-dark.css";
-  /*
   home.file.".config/gtk-4.0/assets" = {
     recursive = true;
     source = "${my_catppuccin}/share/themes/${my_catppuccin_name}/gtk-4.0/assets";
   };
+  /*
   # */
 
   /* Now symlink the `~/.config/gtk-4.0/` folder declaratively
