c8d87e848c51d5e7b1bd1abad5d14414a44028d7
shivajreddy <shivajreddy@outlook.com>
2024-04-22 08:28:15 -0400

    .

--- home/apps/neovim/lua/config/keymaps.lua
+++ home/apps/neovim/lua/config/keymaps.lua
@@ -6,8 +6,16 @@ vim.keymap.set("n", "<C-n>", "<cmd>Neotree toggle<CR>", { desc = "Neotree Toggle
 
 vim.api.nvim_set_keymap("n", "<leader>as", ":ASToggle<CR>", {})
 
+--[[ :: Using Smart-splits ::
 vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
 vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
 vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
 vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
 vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
+-- ]]
+
+-- Using TmuxNavigator
+vim.keymap.set("n", "<C-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", {})
+vim.keymap.set("n", "<C-j>", "<cmd><C-U>TmuxNavigateDown<cr>", {})
+vim.keymap.set("n", "<C-k>", "<cmd><C-U>TmuxNavigateUp<cr>", {})
+vim.keymap.set("n", "<C-l>", "<cmd><C-U>TmuxNavigateRight<cr>", {})
