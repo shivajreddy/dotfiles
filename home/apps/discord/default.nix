0cf1d1b110429de63db1d557a98a61855522ed1f
shivajreddy <shivajreddy@outlook.com>
2024-04-22 15:22:16 -0400

    .

--- home/apps/discord/default.nix
+++ home/apps/discord/default.nix
@@ -16,6 +16,6 @@ in
 
   xdg.configFile."WebCord/Themes/CatpuccinMocha".source = ./mocha.theme.css;
 
-  home.file.".local/share/applications/chrome.desktop".source = discordDesktopFile;
+  home.file.".local/share/applications/discord.desktop".source = discordDesktopFile;
 
 }
