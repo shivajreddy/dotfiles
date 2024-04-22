6d43750648efd1e5ba4768bb159f29a9f9909312
shivajreddy <shivajreddy@outlook.com>
2024-04-22 15:19:18 -0400

    .

--- home/apps/discord/default.nix
+++ home/apps/discord/default.nix
@@ -1,6 +1,22 @@
 { config, inputs, pkgs, ... }:
+let 
+    discordDesktopFile = pkgs.writeTextFile {
+    name = "discord.desktop";
+    destination = "/share/applications/discord.desktop";
+    text = ''
+      [Desktop Entry]
+      Type=Application
+      Name=Discord
+      Exec=webcord
+      Icon=~/dotfiles/home/apps/discord/icon.png
+    '';
+  };
+in
 {
 
   xdg.configFile."WebCord/Themes/CatpuccinMocha".source = ./mocha.theme.css;
+
+
+  home.file.".local/share/applications/chrome.desktop".source = chromeDesktopFile;
 
 }
