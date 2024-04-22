b9efb0ff70986004a6814da189719046a30ec681
shivajreddy <shivajreddy@outlook.com>
2024-04-22 12:27:30 -0400

    .

--- home/themes/gtk/gtk.nix
+++ home/themes/gtk/gtk.nix
@@ -78,10 +78,9 @@ in
 
   };
 
-  # /*
-  # home.file.".config/gtk-4.0/gtk.css".source = "${my_catppuccin}/share/themes/${my_catppuccin_name}/gtk-4.0/gtk.css";
-  # home.file.".config/gtk-4.0/gtk-dark.css".source = "${my_catppuccin}/share/themes/${my_catppuccin_name}/gtk-4.0/gtk-dark.css";
-
+  home.file.".config/gtk-4.0/gtk.css".source = "${my_catppuccin}/share/themes/${my_catppuccin_name}/gtk-4.0/gtk.css";
+  home.file.".config/gtk-4.0/gtk-dark.css".source = "${my_catppuccin}/share/themes/${my_catppuccin_name}/gtk-4.0/gtk-dark.css";
+  /*
   home.file.".config/gtk-4.0/assets" = {
     recursive = true;
     source = "${my_catppuccin}/share/themes/${my_catppuccin_name}/gtk-4.0/assets";
