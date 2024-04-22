62ac917b2bc4fbb463120106413af689e7fe4906
shivajreddy <shivajreddy@outlook.com>
2024-04-22 19:38:33 -0400

    .

--- home/apps/hyprlock/default.nix
+++ home/apps/hyprlock/default.nix
@@ -3,7 +3,11 @@
 {
 	imports = [];
 
-  xdg.configFile."/hypr/mocha.conf".source = ./mocha.conf;
-  xdg.configFile."/hypr/hyprlock.conf".source = ./hyprlock.conf;
+  # xdg.configFile."/hypr/mocha.conf".source = ./mocha.conf;
+  # xdg.configFile."/hypr/hyprlock.conf".source = ./hyprlock.conf;
+
+  home.file.".config/hypr/mocha.conf".source = ./mocha.conf;
+  home.file.".config/hypr/hyprlock.conf".source = ./hyprlock.conf;
+
 }
 
