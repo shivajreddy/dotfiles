f7847508ca15bf2e7c138957f7533379f43bb038
shivajreddy <shivajreddy@outlook.com>
2024-04-23 17:09:11 -0400

    Update dotfiles

--- home/apps/tmux/tmux.nix
+++ home/apps/tmux/tmux.nix
@@ -14,33 +14,51 @@ in
 
       tmuxPlugins.sensible
       tmuxPlugins.yank
-      tmuxPlugins.resurrect
-      tmuxPlugins.continuum
       tmuxPlugins.tmux-thumbs
       tmuxPlugins.tmux-fzf
       tmuxPlugins.fzf-tmux-url
 
       {
         plugin = tmuxPlugins.catppuccin;
-        /*
         extraConfig = ''
-          set -g @catppuccin_falvour 'mocha'
-          set -g @catppuccin_window_tabs_enabled on
-
-          set -g @catppuccin_window_right_separator "█ "
-          set -g @catppuccin_window_number_position "right"
-          set -g @catppuccin_window_middle_separator " | "
-          set -g @catppuccin_window_default_fill "none"
-          set -g @catppuccin_window_current_fill "all"
-          set -g @catppuccin_status_modules_right "application session user host date_time"
-          set -g @catppuccin_status_left_separator "█"
-          set -g @catppuccin_status_right_separator "█"
-          set -g @catppuccin_date_time_text "%Y-%m-%d %H:%M:%S"
-        '';
-        */
-        extraConfig = ''
+        set -g @catppuccin_window_left_separator ""
+        set -g @catppuccin_window_right_separator " "
+        set -g @catppuccin_window_middle_separator " █"
+        set -g @catppuccin_window_number_position "right"
+        set -g @catppuccin_window_default_fill "number"
+        set -g @catppuccin_window_default_text "#W"
+        set -g @catppuccin_window_current_fill "number"
+        set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
+        set -g @catppuccin_status_modules_right "directory meetings date_time"
+        set -g @catppuccin_status_modules_left "session"
+        set -g @catppuccin_status_left_separator  " "
+        set -g @catppuccin_status_right_separator " "
+        set -g @catppuccin_status_right_separator_inverse "no"
+        set -g @catppuccin_status_fill "icon"
+        set -g @catppuccin_status_connect_separator "no"
+        set -g @catppuccin_directory_text "#{b:pane_current_path}"
+        set -g @catppuccin_date_time_text "%H:%M"
         '';
       }
+
+      {
+        plugin = tmuxPlugins.resurrect;
+        extraConfig = ''
+        set -g @resurrect-strategy-vim 'session'
+        set -g @resurrect-strategy-nvim 'session'
+        set -g @resurrect-capture-pane-contents 'on'
+        '';
+      }
+
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+        set -g @continuum-restore 'on'
+        set -g @continuum-boot 'on'
+        set -g @continuum-save-interval '10'
+        '';
+      }
+
     ];
 
     extraConfig = ''
