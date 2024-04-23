b6142533975611ef1f63cc405033f68d3fd485f5
shivajreddy <shivajreddy@outlook.com>
2024-04-23 16:30:31 -0400

    Update dotfiles

--- home/apps/tmux/tmux.nix
+++ home/apps/tmux/tmux.nix
@@ -17,6 +17,7 @@ in
       tmuxPlugins.vim-tmux-navigator
       {
         plugin = tmuxPlugins.catppuccin;
+        /*
         extraConfig = ''
           set -g @catppuccin_falvour 'mocha'
           set -g @catppuccin_window_tabs_enabled on
@@ -30,6 +31,27 @@ in
           set -g @catppuccin_status_left_separator "█"
           set -g @catppuccin_status_right_separator "█"
           set -g @catppuccin_date_time_text "%Y-%m-%d %H:%M:%S"
+        '';
+        */
+        extraConfig = ''
+          set -g @catppuccin_window_left_separator ""
+          set -g @catppuccin_window_right_separator " "
+          set -g @catppuccin_window_middle_separator " █"
+          set -g @catppuccin_window_number_position "right"
+
+          set -g @catppuccin_window_default_fill "number"
+          set -g @catppuccin_window_default_text "#W"
+
+          set -g @catppuccin_window_current_fill "number"
+          set -g @catppuccin_window_current_text "#W"
+
+          set -g @catppuccin_status_modules_right "directory user host session"
+          set -g @catppuccin_status_left_separator  " "
+          set -g @catppuccin_status_right_separator ""
+          set -g @catppuccin_status_fill "icon"
+          set -g @catppuccin_status_connect_separator "no"
+
+          set -g @catppuccin_directory_text "#{pane_current_path}"
         '';
       }
     ];
