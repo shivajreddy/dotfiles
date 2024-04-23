a22ca23b575a5845f3bc0eb0d0e8c21deadbc22f
shivajreddy <shivajreddy@outlook.com>
2024-04-23 15:26:48 -0400

    .

--- home/apps/zsh/scripts/rebuild.sh
+++ home/apps/zsh/scripts/rebuild.sh
@@ -1,11 +1,8 @@
 #! /usr/bin/env bash
 
 # Rebuild NixOS configuration based on the current hostname's flake
-rebuild() {
-    local hostname
-    hostname=$(hostname)  # Capture the hostname in a variable
-    echo "Home-Manager-Rebuild for: $hostname"
+local hostname
+hostname=$(hostname)  # Capture the hostname in a variable
+echo "Home-Manager-Rebuild for: $hostname"
 
-    home-manager switch --flake "$HOME/dotfiles"
-}
-
+home-manager switch --flake "$HOME/dotfiles"
