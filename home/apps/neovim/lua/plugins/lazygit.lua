bf6c1ba4664da3b642fd0337477377584205551c
shivajreddy <shivajreddy@outlook.com>
2024-04-23 07:22:43 -0400

    .

--- home/apps/neovim/lua/plugins/lazygit.lua
+++ home/apps/neovim/lua/plugins/lazygit.lua
@@ -12,7 +12,7 @@ return {
 		"nvim-lua/plenary.nvim",
 	},
 	-- setting the keybinding for LazyGit with 'keys' is recommended in
-	-- order to load the plugin when the command is run for the first time
+	-- order to load the plugin when the command is run for the first time.
 	keys = {
 		{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
 	},
