62ff60f6d9a3f0460e046df3a3ff5ce54d09e0ed
shivajreddy <shivajreddy@outlook.com>
2024-04-23 16:49:35 -0400

    Update dotfiles

--- home/apps/tmux/tmux.nix
+++ home/apps/tmux/tmux.nix
@@ -8,13 +8,21 @@ in
   programs.tmux = {
     enable = true;
 
-    extraConfig = ''
-    ${tmux-conf}
-    '';
-
     plugins = with pkgs; [
       tmuxPlugins.better-mouse-mode
       tmuxPlugins.vim-tmux-navigator
+
+
+      tmuxPlugins.sensible
+      tmuxPlugins.yank
+      tmuxPlugins.resurrect
+      tmuxPlugins.continuum
+      tmuxPlugins.tmux-thumbs
+      tmuxPlugins.tmux-fzf
+      tmuxPlugins.fzf-tmux-url
+      set -g @plugin 'omerxx/tmux-sessionx'
+
+
       {
         plugin = tmuxPlugins.catppuccin;
         /*
@@ -34,27 +42,39 @@ in
         '';
         */
         extraConfig = ''
-          set -g @catppuccin_window_left_separator ""
-          set -g @catppuccin_window_right_separator " "
-          set -g @catppuccin_window_middle_separator " █"
-          set -g @catppuccin_window_number_position "right"
-
-          set -g @catppuccin_window_default_fill "number"
-          set -g @catppuccin_window_default_text "#W"
-
-          set -g @catppuccin_window_current_fill "number"
-          set -g @catppuccin_window_current_text "#W"
-
-          set -g @catppuccin_status_modules_right "directory user host session"
-          set -g @catppuccin_status_left_separator  " "
-          set -g @catppuccin_status_right_separator ""
-          set -g @catppuccin_status_fill "icon"
-          set -g @catppuccin_status_connect_separator "no"
-
-          set -g @catppuccin_directory_text "#{pane_current_path}"
         '';
       }
     ];
 
+    extraConfig = ''
+    ${tmux-conf}
+      set -g @sessionx-bind 'o'
+      set -g @sessionx-x-path '~/dotfiles'
+      set -g @sessionx-window-height '85%'
+      set -g @sessionx-window-width '75%'
+      set -g @sessionx-zoxide-mode 'on'
+      set -g @sessionx-filter-current 'false'
+      set -g @sessionx-preview-enabled 'true'
+      set -g @continuum-restore 'on'
+      set -g @resurrect-strategy-nvim 'session'
+      set -g @catppuccin_window_left_separator ""
+      set -g @catppuccin_window_right_separator " "
+      set -g @catppuccin_window_middle_separator " █"
+      set -g @catppuccin_window_number_position "right"
+      set -g @catppuccin_window_default_fill "number"
+      set -g @catppuccin_window_default_text "#W"
+      set -g @catppuccin_window_current_fill "number"
+      set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
+      set -g @catppuccin_status_modules_right "directory meetings date_time"
+      set -g @catppuccin_status_modules_left "session"
+      set -g @catppuccin_status_left_separator  " "
+      set -g @catppuccin_status_right_separator " "
+      set -g @catppuccin_status_right_separator_inverse "no"
+      set -g @catppuccin_status_fill "icon"
+      set -g @catppuccin_status_connect_separator "no"
+      set -g @catppuccin_directory_text "#{b:pane_current_path}"
+      set -g @catppuccin_meetings_text "#($HOME/.config/tmux/scripts/cal.sh)"
+      set -g @catppuccin_date_time_text "%H:%M"
+    '';
   };
 }
