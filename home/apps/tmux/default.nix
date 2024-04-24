672b684a6ac1299eb1e1fe01fd1d957ab8c896f3
shivajreddy <shivajreddy@outlook.com>
2024-04-24 07:47:33 -0400

    Update dotfiles

--- home/apps/tmux/default.nix
+++ home/apps/tmux/default.nix
@@ -26,7 +26,8 @@ in
         extraConfig = ''
         set -g @catppuccin_window_left_separator ""
         set -g @catppuccin_window_right_separator " "
-        set -g @catppuccin_window_middle_separator " █"
+        # set -g @catppuccin_window_middle_separator " █"
+        # set -g @catppuccin_window_middle_separator ""
         set -g @catppuccin_window_number_position "right"
 
         set -g @catppuccin_window_default_fill "none" # number all none
