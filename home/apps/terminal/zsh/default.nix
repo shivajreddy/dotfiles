6fbe1fa70ba0a5cb5c89412ea095ed6306e0fc34
shivajreddy <shivajreddy@outlook.com>
2024-04-28 04:54:58 -0400

    Update dotfiles

--- home/apps/terminal/zsh/default.nix
+++ home/apps/terminal/zsh/default.nix
@@ -33,18 +33,10 @@ in {
       zstyle ':completion:*' matcher-list "" 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
       PROMPT="%U%F{magenta}%n%f%u@%U%F{blue}%m%f%u:%F{yellow}%~%f
        %F{green}→%f "
-
-      # [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
-
-      # current wordchars
-      # *?_-.[]~=/&;!#$%^(){}<>
-
       WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
-
-
       ${zsh_scripts}
-
     '';
+      # [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
   };
 
 
