e9182e55ace505b0293901e0365825aff782fd5d
shivajreddy <shivajreddy@outlook.com>
2024-05-02 09:19:09 -0400

    Update dotfiles

--- home/apps/git/default.nix
+++ home/apps/git/default.nix
@@ -1,6 +1,5 @@
-{ lib, ... }:
+{ ... }:
 {
-  imports = [];
 
   programs.git = {
     enable = true;
@@ -9,6 +8,11 @@
     extraConfig.credential.helper = "store";
     # Note: first time using git, when pushing to your account,
     # use username, for password paste the token from github.com 
+
+    extraConfig = {
+      http.postBuffer = "2147483648"; # 2 GB
+    };
+
   };
 
   programs.lazygit.enable = true;
