6393f0ba6d18d7d2ce129079daa249b48e97ab52
shivajreddy <shivajreddy@outlook.com>
2024-05-06 09:19:58 -0400

    Update dotfiles

--- home/fonts/default.nix
+++ home/fonts/default.nix
@@ -2,4 +2,6 @@
 {
   home.file.".local/share/fonts/berkeley-mono".source = ./berkeley-mono;
   home.file.".local/share/fonts/berkeley-mono-nerdfont".source = ./berkeley-mono-nerdfont;
+
+  home.file.".local/share/fonts/monolisa".source = ./monolisa;
 }
