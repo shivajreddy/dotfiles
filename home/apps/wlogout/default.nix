51e6a15dc150d7fd1f49c19e709b4a23bc529460
shivajreddy <shivajreddy@outlook.com>
2024-05-11 00:39:52 -0400

    Update dotfiles

--- home/apps/wlogout/default.nix
+++ home/apps/wlogout/default.nix
@@ -5,6 +5,8 @@
 	programs.wlogout.enable = true;
 
   # Copy the theme folder 
-  # xdg.configFile."kitty/themes".source = ./themes;
+  xdg.configFile."wlogout/icons".source = ./icons;
+  xdg.configFile."wlogout/layout".source = ./layout;
+  xdg.configFile."wlogout/style.css".source = ./style.css;
 }
 
