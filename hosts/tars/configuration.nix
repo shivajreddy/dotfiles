c73537839dd77e15a8e6515cc7e3b3c538d1fe9e
shivajreddy <shivajreddy@outlook.com>
2024-05-16 21:23:37 -0400

    Update dotfiles

--- hosts/tars/configuration.nix
+++ hosts/tars/configuration.nix
@@ -68,18 +68,7 @@
 
   hardware.bluetooth = {
     enable = true;
-    powerOnBoot = true;
-        settings = {
-      General = {
-        Name = "Hello";
-        ControllerMode = "dual";
-        FastConnectable = "true";
-        Experimental = "true";
-      };
-      Policy = {
-        AutoEnable = "true";
-      };
-    };
+    # powerOnBoot = true;
     # settings = {
     #  General = {
     #    ControllerMode = "bredr";
