7cb13e411baa89a5f68aaa9d99308ce636c5112e
shivajreddy <shivajreddy@outlook.com>
2024-05-21 17:28:08 -0400

    Update dotfiles

--- home/apps/browsers/firefox/default.nix
+++ home/apps/browsers/firefox/default.nix
@@ -3,7 +3,7 @@
 let
     # user_chrome_css = builtins.readFile (./userChrome.css);
     # user_chrome_css = builtins.readFile (./myUserChrome.css);
-    user_chrome_css = builtins.readFile (./arc.css);
+    # user_chrome_css = builtins.readFile (./arc.css);
 
     user_js = builtins.readFile (./user.js);
 in
@@ -13,9 +13,10 @@ in
   programs.firefox = {
     enable = true;
     profiles."shiva" = {
-      userChrome = ''
-      ${user_chrome_css}
-      '';
+
+      # userChrome = ''
+      # ${user_chrome_css}
+      # '';
 
       extraConfig = ''
       ${user_js}
