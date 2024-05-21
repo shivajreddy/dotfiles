4ed16eb15f7703f24dc331a7441c677aff4bc9ec
shivajreddy <shivajreddy@outlook.com>
2024-05-21 12:28:15 -0400

    Update dotfiles

--- home/apps/browsers/firefox/default.nix
+++ home/apps/browsers/firefox/default.nix
@@ -1,7 +1,8 @@
 {pkgs, inputs, ...}:
 
 let
-    user_chrome_css = builtins.readFile (./userChrome.css);
+    # user_chrome_css = builtins.readFile (./userChrome.css);
+    user_chrome_css = builtins.readFile (./myUserChrome.css);
 in
 
 {
