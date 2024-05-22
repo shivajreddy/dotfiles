ed658f139d1cc070d5ec5e8aff2a3a72dfbaebfd
shivajreddy <shivajreddy@outlook.com>
2024-05-22 13:18:53 -0400

    Update dotfiles

--- home/apps/browsers/firefox/default.nix
+++ home/apps/browsers/firefox/default.nix
@@ -5,7 +5,9 @@ let
     # user_chrome_css = builtins.readFile (./myUserChrome.css);
     # user_chrome_css = builtins.readFile (./arc.css);
 
-    user_js = builtins.readFile (./user.js);
+    # user_js = builtins.readFile (./user.js);
+
+    # Trying css-themes from https://firefoxcss-store.github.io/
 in
 
 {
