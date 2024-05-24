7a3cdccf69c6864f70b9cd7a8508744bac149e30
shivajreddy <shivajreddy@outlook.com>
2024-05-24 17:30:16 -0400

    Update dotfiles

--- home/apps/browsers/firefox/default.nix
+++ home/apps/browsers/firefox/default.nix
@@ -2,6 +2,7 @@
 
 let
     bottomtabs_css = builtins.readFile (./bottomtabs.css);
+    arc_css = builtins.readFile (./arc.css);
     # user_chrome_css = builtins.readFile (./myUserChrome.css);
     # user_chrome_css = builtins.readFile (./arc.css);
 
@@ -17,9 +18,9 @@ in
     profiles."shiva" = {
 
       # enable this by about:config > search 'userprof' > toolkit.legacyUserProfileCustomizations.stylesheets
-      # userChrome = ''
-      # ${bottomtabs_css}
-      # '';
+      userChrome = ''
+      ${arc_css}
+      '';
 
       # extraConfig = ''
       # ${user_js}
