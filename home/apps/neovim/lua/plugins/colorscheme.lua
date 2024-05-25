26f92d8d370df90d004ada3a7b08618184a3b02b
shivajreddy <shivajreddy@outlook.com>
2024-05-25 18:32:04 -0400

    Update dotfiles

--- home/apps/neovim/lua/plugins/colorscheme.lua
+++ home/apps/neovim/lua/plugins/colorscheme.lua
@@ -191,12 +191,44 @@ return {
 		},
 	},
 
+	-- :: GRUVBOX ::
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		config = true,
+		opts = {
+			terminal_colors = true, -- add neovim terminal colors
+			undercurl = true,
+			underline = true,
+			bold = true,
+			italic = {
+				strings = true,
+				emphasis = true,
+				comments = true,
+				operators = false,
+				folds = true,
+			},
+			strikethrough = true,
+			invert_selection = false,
+			invert_signs = false,
+			invert_tabline = false,
+			invert_intend_guides = false,
+			inverse = true, -- invert background for search, diffs, statuslines and errors
+			contrast = "", -- can be "hard", "soft" or empty string
+			palette_overrides = {},
+			overrides = {},
+			dim_inactive = false,
+			transparent_mode = false,
+		},
+	},
+
 	-- Configure LazyVim to load the colorscheme
 	{
 		"LazyVim/LazyVim",
 		opts = {
 			-- colorscheme = "catppuccin",
-			colorscheme = "rose-pine-main", -- rose-pine-main rose-pine-moon  rose-pine-dawn
+			-- colorscheme = "rose-pine-main", -- rose-pine-main rose-pine-moon  rose-pine-dawn
+			colorscheme = "gruvbox",
 			-- colorscheme = "tokyonight",
 		},
 	},
