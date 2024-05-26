ddc11baf3ef089fcb7fb4eb9a9d63caa7f112675
shivajreddy <shivajreddy@outlook.com>
2024-05-25 23:05:27 -0400

    Update dotfiles

--- home/apps/tmux/scripts/truncate_path.sh
+++ home/apps/tmux/scripts/truncate_path.sh
@@ -13,24 +13,20 @@ fi
 echo $path
 '
 
-
-# Variables
 path="$1"
+user_home="/home/shiva"
+
+# Replace $user_home with ~ in the path
+path="${path/#$user_home/\~}"
+
 fixed_length=$2
-username="shiva"
-home_dir="/home/$username"
 
-# Replace /home/<user> with ~
-path="${path/#$home_dir/~}"
-
-# Check if path length exceeds the fixed length
-if [ ${#path} -gt $fixed_length ]; then
+if (( ${#path} > $fixed_length )); then
     path=$(echo "$path" | tail -c "$fixed_length")
-    if ! [[ "$path" =~ ^//* ]]; then
-        path="/${path#*/}"
+    if ! [[ "$path" =~ ^//\* ]]; then
+        path="/${path#\*/}"
     fi
     path="...$path"
 fi
 
-# Output the result
 echo $path
