a68dcfdd6bc6f26422dfe4d8fad32a804a976d7a
Shiva <59872081+shivajreddy@users.noreply.github.com>
2024-06-26 09:13:19 -0400

    Update wsl colorscheme.lua

--- windows/wsl/.config/nvim/lua/plugins/colorscheme.lua
+++ windows/wsl/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,25 @@
+-- rose useful link
+-- https://www.reddit.com/r/neovim/comments/19f7s7e/changing_default_ros%C3%A9_pine_colours/
 return {
   {
     "rose-pine/neovim",
     name = "rose-pine",
+    config = function()
+      require('rose-pine').setup({
+        styles = {
+          bold = true,
+          italic = true,
+          transparency = true
+        },
+        groups = {
+          h1 = "love"
+        },
+        highlight_groups = {
+          -- Normal = { bg = "#061111" },
+          -- ["String"] = { fg = "#27d653" },
+        }
+      })
+    end
   },
 
   {
