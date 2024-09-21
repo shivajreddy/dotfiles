4891e4bef898f2d7683df408fd5e76d8e91cd477
shiva <shivajreddy@outlook.com>
2024-09-21 17:02:56 -0400

    Updated wezterm

--- common/wezterm/wezterm.lua
+++ common/wezterm/wezterm.lua
@@ -50,8 +50,8 @@ end
 -- config_keys.apply(c)
 
 c.font = wezterm.font_with_fallback({
-	"JetBrains Mono",
-	-- "Berkeley Mono",
+	-- "JetBrains Mono",
+	"Berkeley Mono",
 	-- "Cascadia Code",
 	-- "IBM Plex Mono",
 	-- "Comic Code Ligatures",
@@ -77,9 +77,10 @@ if utils.is_windows() then
 	c.font_size = 12
 end
 
-print(wezterm.target_triple:find("windows"))
-print(wezterm.target_triple:find("darwin"))
-print(wezterm.target_triple:find("linux"))
+c.hide_tab_bar_if_only_one_tab = false
+c.tab_bar_at_bottom = true
+c.use_fancy_tab_bar = false
+c.tab_and_split_indices_are_zero_based = true
 
 -- https://wezfurlong.org/wezterm/config/lua/config/window_decorations.html?h=window_de
 -- c.window_decorations = "NONE"
@@ -111,13 +112,77 @@ c.colors = {
 	background = "#09080C",
 	cursor_fg = "#191724",
 	cursor_bg = "#ebbcba",
-}
 
--- c.hide_tab_bar_if_only_one_tab = false
-c.tab_bar_at_bottom = true
--- c.use_fancy_tab_bar = true
-c.use_fancy_tab_bar = false
-c.tab_and_split_indices_are_zero_based = true
+	-- tab_bar = {
+	-- 	-- The color of the inactive tab bar edge/divider
+	-- 	inactive_tab_edge = "#575757",
+	-- },
+
+	tab_bar = {
+		-- The color of the strip that goes along the top of the window
+		-- (does not apply when fancy tab bar is in use)
+		background = "#121018",
+
+		-- The active tab is the one that has focus in the window
+		active_tab = {
+			-- The color of the background area for the tab
+			bg_color = "#09080C",
+			fg_color = "#c0c0c0",
+
+			-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+			-- label shown for this tab.
+			-- The default is "Normal"
+			intensity = "Normal",
+
+			-- Specify whether you want "None", "Single" or "Double" underline for
+			-- label shown for this tab.
+			-- The default is "None"
+			underline = "None",
+
+			-- Specify whether you want the text to be italic (true) or not (false)
+			-- for this tab.  The default is false.
+			italic = false,
+
+			-- Specify whether you want the text to be rendered with strikethrough (true)
+			-- or not for this tab.  The default is false.
+			strikethrough = false,
+		},
+
+		-- Inactive tabs are the tabs that do not have focus
+		inactive_tab = {
+			bg_color = "#1C1825",
+			fg_color = "#808080",
+
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `inactive_tab`.
+		},
+
+		-- You can configure some alternate styling when the mouse pointer
+		-- moves over inactive tabs
+		inactive_tab_hover = {
+			bg_color = "#1C1825",
+			fg_color = "#808080",
+			italic = false,
+		},
+
+		-- The new tab button that let you create new tabs
+		new_tab = {
+			bg_color = "#09080C",
+			fg_color = "#c0c0c0",
+
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `new_tab`.
+		},
+
+		-- You can configure some alternate styling when the mouse pointer
+		-- moves over the new tab button
+		new_tab_hover = {
+			bg_color = "#09080C",
+			fg_color = "#c0c0c0",
+			italic = false,
+		},
+	},
+}
 
 local function get_current_working_dir(tab)
 	local current_dir = tab.active_pane.current_working_dir
