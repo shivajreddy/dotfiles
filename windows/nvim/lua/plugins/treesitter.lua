a808017172a63c1ac1bbcceed3782991e639b082
shiva <shivajreddy@outlook.com>
2024-09-26 19:38:09 -0400

    .

--- windows/nvim/lua/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-
-  "nvim-treesitter",
-  ensure_installed = { "c" }, -- Add other languages as needed
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    additional_vim_regex_highlighting = false,
-  },
-}
