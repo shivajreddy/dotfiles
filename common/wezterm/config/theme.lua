b8704ce8602e39a6e121564e83c0cd64c0c9725c
shiva <smbp@shiva.local>
2024-09-29 17:17:37 -0400

    .

--- common/wezterm/config/theme.lua
+++ common/wezterm/config/theme.lua
@@ -18,31 +18,22 @@ function M.apply(c)
 		tab_bar = {
 			-- The color of the strip that goes along the top of the window
 			-- (does not apply when fancy tab bar is in use)
-			background = "#121018",
+			background = "#1f1d2e",
+			-- background = "#121018",
 
 			-- The active tab is the one that has focus in the window
 			active_tab = {
-				-- The color of the background area for the tab
-				bg_color = "#09080C",
-				fg_color = "#c0c0c0",
+				-- bg_color = "#09080C",
+				-- bg_color = "#11111b",
+				bg_color = "#1f1d2e",
+				-- fg_color = "#c0c0c0",
+				fg_color = "#ebbcba",
 
-				-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-				-- label shown for this tab.
-				-- The default is "Normal"
-				intensity = "Normal",
-
-				-- Specify whether you want "None", "Single" or "Double" underline for
-				-- label shown for this tab.
-				-- The default is "None"
+				-- intensity = "Normal",
+				intensity = "Bold",
 				underline = "None",
-
-				-- Specify whether you want the text to be italic (true) or not (false)
-				-- for this tab.  The default is false.
-				italic = false,
-
-				-- Specify whether you want the text to be rendered with strikethrough (true)
-				-- or not for this tab.  The default is false.
 				strikethrough = false,
+				italic = false,
 			},
 
 			-- Inactive tabs are the tabs that do not have focus
