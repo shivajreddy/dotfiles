2d5c1e89a5606e5263b1dd7d3580802648886b1f
shiva <shivajreddy@outlook.com>
2024-10-02 15:57:51 -0400

    .

--- wsl/nvim/lua/plugins/lsp.lua
+++ wsl/nvim/lua/plugins/lsp.lua
@@ -8,7 +8,7 @@ return {
       -- inlay_hints = { enabled = true }, -- this is deprecated, use this instead? inlay_hint.enable(),
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
+        -- pyright = {},
         -- ruff = {},
         clangd = {
           keys = {
