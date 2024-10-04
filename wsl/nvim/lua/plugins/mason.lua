604d742541b606c2550a75f5f340a3f2410fde64
shiva <shivajreddy@outlook.com>
2024-10-03 21:10:05 -0400

    .

--- wsl/nvim/lua/plugins/mason.lua
+++ wsl/nvim/lua/plugins/mason.lua
@@ -2,6 +2,7 @@ return {
   "williamboman/mason.nvim",
   opts = function(_, opts)
     table.insert(opts.ensure_installed, "pyright")
-    table.insert(opts.ensure_installed, "black")
+    -- table.insert(opts.ensure_installed, "black")
+    table.insert(opts.ensure_installed, "ruff")
   end,
 }
