50a8e24623de5ba4acdd380d5bcbf95638d0ada5
shiva <shivajreddy@outlook.com>
2024-10-05 12:55:32 -0400

    .

--- common/wezterm/config/tabbar.lua
+++ common/wezterm/config/tabbar.lua
@@ -11,7 +11,6 @@ function M.apply(c)
 
 	c.window_frame = {
 		font = require("wezterm").font("Berkeley Mono"),
-		font_size = 11.0,
 		inactive_titlebar_bg = "#1f1d2e",
 		active_titlebar_bg = "#1f1d2e",
 		-- inactive_titlebar_bg = "#353535",
