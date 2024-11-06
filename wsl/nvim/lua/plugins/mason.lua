0791db640889125840c37c2786f0f416bbd046f7
shiva <shivajreddy@outlook.com>
2024-11-06 16:30:31 -0500

    .

--- wsl/nvim/lua/plugins/mason.lua
+++ wsl/nvim/lua/plugins/mason.lua
@@ -3,7 +3,7 @@ return {
   opts = function(_, opts)
     table.insert(opts.ensure_installed, "pyright")
     -- table.insert(opts.ensure_installed, "black")
-    table.insert(opts.ensure_installed, "ruff")
+    -- table.insert(opts.ensure_installed, "ruff")
     table.insert(opts.ensure_installed, "prettier")
   end,
 }
