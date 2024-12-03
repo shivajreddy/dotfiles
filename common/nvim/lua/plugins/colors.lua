4b17ead08b2e7fe560a44220d2475a0216fdf057
shiva <shivajreddy@outlook.com>
2024-12-03 07:44:32 -0500

    .

--- common/nvim/lua/plugins/colors.lua
+++ common/nvim/lua/plugins/colors.lua
@@ -1,3 +1,4 @@
+--[[
 return {
   {
     "erikbackman/brightburn.vim",
@@ -12,7 +13,7 @@ return {
 
         styles = {
           bold = true,
-          italic = false,
+          italic = true,
           transparency = true, -- Must not set any color
         },
         -- in repo: https://github.dev/rose-pine/neovim, the file: rose-pine.lua
@@ -33,6 +34,42 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "rose-pine",
+    },
+  },
+}
+]]
+
+return {
+  {
+    "metalelf0/base16-black-metal-scheme",
+    name = "metalelf0",
+    config = function()
+      require().setup({
+        variant = "auto", -- auto, main, moon, or dawn
+
+        styles = {
+          bold = true,
+          italic = true,
+          transparency = true, -- Must not set any color
+        },
+        -- in repo: https://github.dev/rose-pine/neovim, the file: rose-pine.lua
+        -- contains all the highlight group names, under the variable `default_highlights`
+        highlight_groups = {
+          -- Normal = { bg = "#0e0a01" }, -- rosepine burnt background
+          CursorLine = { bg = "none" }, -- #0e0a01 base  #121F2B none
+          ColorColumn = { bg = "#121F2B" }, -- #0e0a01
+          SignColumn = { bg = "#31748f", fg = "#f6c177" },
+          LineNr = { fg = "#413630" },
+          CursorLineNr = { fg = "#908caa" }, -- rosepine subtle
+        },
+      })
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "base16-black-metal-venom",
     },
   },
 }
