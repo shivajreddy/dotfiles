d68c666ff4881f17d4d27f5b3a74cba0f3e5121b
shiva <shivajreddy@outlook.com>
2024-12-12 20:44:45 -0500

    .

--- common/nvim/lua/plugins/bufferline.lua
+++ common/nvim/lua/plugins/bufferline.lua
@@ -1,40 +1,14 @@
--- local mocha = require("catppuccin.palettes").get_palette("mocha")
-
 local bufferline = require("bufferline")
-local mocha = require("catppuccin.palettes").get_palette("mocha")
 
 return {
   "akinsho/bufferline.nvim",
-  event = "VeryLazy",
   opts = {
     options = {
-      -- diagnostics = "nvim_lsp",
-      -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      --   local icon = level:match("error") and "󰅙 " or " "
-      --   return " " .. icon .. count
-      -- end,
-      themable = true,
-      separator_style = "thick", -- "thick"  "slant"
-      -- indicator = { style = "underline" },
-      always_show_bufferline = false,
-      style_preset = bufferline.style_preset.default,
-      -- style_preset = bufferline.style_preset.no_italic,
       show_buffer_close_icons = false,
-    },
-
-    highlights = require("catppuccin.groups.integrations.bufferline").get({
-      styles = { "bold" },
-      custom = {
-        all = {
-          fill = { bg = "#000000" },
-        },
-        mocha = {
-          background = { fg = mocha.text },
-        },
-        latte = {
-          background = { fg = "#000000" },
-        },
+      style_preset = {
+        bufferline.style_preset.no_italic,
+        bufferline.style_preset.minimal,
       },
-    }),
+    },
   },
 }
