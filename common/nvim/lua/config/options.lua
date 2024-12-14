0daad117aae28e48fab332565884be1bec9ef11a
shiva <shivajreddy@outlook.com>
2024-12-14 16:05:26 -0500

    .

--- common/nvim/lua/config/options.lua
+++ common/nvim/lua/config/options.lua
@@ -56,11 +56,11 @@ vim.g.vsvim_hidemarks = "<>[]^.'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV
 
 -- White Space
 vim.opt.list = false
-vim.opt.listchars = {
-  space = ".", -- Show spaces as dots
-  tab = "»·", -- Show tabs with a special character and space
-  trail = "·", -- Show trailing spaces
-  extends = "→", -- Show character for text that extends beyond the window
-  precedes = "←", -- Show character for text that precedes the window
-  nbsp = "␣", -- Show non-breaking space
-}
+-- vim.opt.listchars = {
+--   space = ".", -- Show spaces as dots
+--   tab = "»·", -- Show tabs with a special character and space
+--   trail = "·", -- Show trailing spaces
+--   extends = "→", -- Show character for text that extends beyond the window
+--   precedes = "←", -- Show character for text that precedes the window
+--   nbsp = "␣", -- Show non-breaking space
+-- }
