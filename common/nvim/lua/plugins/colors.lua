8e8a3b44b9ce405e0a5647e0c03b7073effbe5c0
smpl <shivajreddy@outlook.com>
2024-12-27 20:47:05 -0500

    .

--- common/nvim/lua/plugins/colors.lua
+++ common/nvim/lua/plugins/colors.lua
@@ -4,7 +4,7 @@ return {
     name = "rose-pine",
     config = function()
       require("rose-pine").setup({
-        variant = "auto", -- auto, main, moon, or dawn
+        variant = "main", -- auto, main, moon, or dawn
 
         palette = {
           main = {
@@ -21,15 +21,15 @@ return {
         },
 
         styles = {
-          bold = false,
-          italic = false,
+          -- bold = false,
+          -- italic = false,
           transparency = true, -- Must not set any color
         },
         -- in repo: https://github.dev/rose-pine/neovim, the file: rose-pine.lua
         -- contains all the highlight group names, under the variable `default_highlights`
         highlight_groups = {
+          Normal = { bg = "#0e0a01" }, -- rosepine burnt background
           --[[
-          -- Normal = { bg = "#0e0a01" }, -- rosepine burnt background
           CursorLine = { bg = "#000000" }, -- #0e0a01 base  #121F2B none   #6e6a86
           ColorColumn = { bg = "#121F2B" }, -- #0e0a01
           SignColumn = { bg = "#31748f", fg = "#f6c177" },
