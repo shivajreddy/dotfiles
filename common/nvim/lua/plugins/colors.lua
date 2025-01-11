cc7b65ff55dd1f5cbc34e1e55aa06ba00c3b2f2f
smpl <shivajreddy@outlook.com>
2025-01-10 19:58:41 -0500

    .

--- common/nvim/lua/plugins/colors.lua
+++ common/nvim/lua/plugins/colors.lua
@@ -147,10 +147,42 @@ return {
     end,
   },
   {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    config = function()
+      -- Default options:
+      require("kanagawa").setup({
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = { -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+          return {}
+        end,
+        theme = "dragon", -- wave dragon lotus
+        background = { -- map the value of 'background' option to a theme
+          dark = "wave", -- try "dragon" !
+          light = "lotus",
+        },
+      })
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
+      colorscheme = "kanagawa",
       -- colorscheme = "rose-pine",
-      colorscheme = "gruber-darker",
+      -- colorscheme = "gruber-darker",
       -- colorscheme = "tokyodark",
       -- colorscheme = "GruberDarker",
       -- colorscheme = "brightburn",
