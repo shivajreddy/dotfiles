34b86c0d650ecf9f4b7b5737ba289532a7508db6
shiva <shivajreddy@outlook.com>
2025-02-18 09:34:33 -0500

    .

--- common/nvim/lua/config/keymaps.lua
+++ common/nvim/lua/config/keymaps.lua
@@ -9,7 +9,7 @@ vim.keymap.set("n", "<F5>", function()
   vim.defer_fn(function()
     -- Type the command and press Enter
     -- local cmd = "clear && ./build.ps1 && ./build/out.exe"  -- for powershell script
-    local cmd = "cls && ./build.bat && ./build/out.exe" -- for cmd prompt script
+    local cmd = "cls && .\\build.bat && .\\build\\out.exe" -- for cmd prompt script
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(cmd .. "<CR>", true, true, true), "n", false)
   end, 100) -- Adjust the delay (in milliseconds) if necessary
 end, { noremap = true, silent = true, desc = "RUN CUSTOM COMMAND" })
