fe8e41464cf0509ee60f5a7714832054bf9576c3
shiva <sreddy@hey.com>
2025-05-04 13:36:42 -0400

    .

---
+++ common/nvim/lua/plugins/mini-comment.lua
@@ -0,0 +1,19 @@
+return {
+  "echasnovski/mini.comment",
+  lazy = false,
+  opts = {
+    mappings = {
+      comment = "<leader>/",
+      comment_visual = "<leader>/",
+      comment_line = "<leader>/",
+      textobject = "<leader>/",
+
+      --[[
+      comment = "<C-/>",
+      comment_line = "<C-/>",
+      comment_visual = "<C-/>",
+      textobject = "<C-/>",
+      --]]
+    },
+  },
+}
---
+++ common/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      -- your explorer configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+
+  keys = {
+    -- disable the keymap to grep files
+    { "<leader>/", false },
+  },
+}
