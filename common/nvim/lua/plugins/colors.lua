b644926b6a6df115e6115bbd75677c43ceb8f035
shiva <sreddy@hey.com>
2025-05-04 15:23:05 -0400

    .

--- common/nvim/lua/plugins/colors.lua
+++ common/nvim/lua/plugins/colors.lua
@@ -238,12 +238,19 @@ return {
         },
         overrides = function(colors) -- add/modify highlights
           return {
+            -- Neovim's color groups
             LineNr = { bg = "#12120F", fg = "#303446" }, -- #1D1C19 Remove bg for transparent
             CursorLineNr = { fg = "#737c73" }, --bg = "#12120F",  #303446 --Remove bg for transparent
             CursorLine = { bg = "#181616" }, -- 0D0C0C 252329 12120F 0e0a01 base 121F2B none 6e6a86
             -- SignColumn = { bg = "#ffffff", fg = "#1f1f1f" },
             Whitespace = { fg = "#26262C" }, --  1f1f28 181921 Add this line to customize the Whitespace highlight group
             WinSeparator = { fg = "#6e6a86", bg = "NONE", bold = true }, -- Customize separator color
+
+            -- Other plugins color groups
+            SnacksPickerList = { bg = "NONE", fg = "#737c73" },
+            SnacksPicker = { bg = "NONE", fg = "#737c73" },
+            SnacksPickerListCursorLine = { fg = "#0d0c0c", bg = "#737c73" },
+            SnacksPickerTree = { fg = "#737c73", bg = "NONE" },
           }
         end,
         theme = "wave", -- wave dragon lotus
