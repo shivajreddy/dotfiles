f75ead7fc41bca920cab04d5447a0ccfd1067681
shiva <shivajreddy@outlook.com>
2025-05-13 08:33:22 -0400

    .

--- common/nvim/lua/plugins/snacks.lua
+++ common/nvim/lua/plugins/snacks.lua
@@ -2,9 +2,9 @@ return {
   "folke/snacks.nvim",
   opts = {
     explorer = {
-      -- your explorer configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+      -- layout = { layout = { position = "right" } },
+      hidden = true,
+      -- ignored = true,
     },
   },
 
