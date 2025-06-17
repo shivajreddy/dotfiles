20ec4838fc5f16b47dfe3da9702896acaeea6561
shiva <sreddy@hey.com>
2025-06-17 08:23:45 -0400

    .

--- common/nvim/lua/plugins/blink_cmp.lua
+++ common/nvim/lua/plugins/blink_cmp.lua
@@ -6,6 +6,7 @@ return {
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- see the "default configuration" section below for full documentation on how to define
     -- your own keymap.
+
     keymap = { preset = "super-tab" },
   },
 }
