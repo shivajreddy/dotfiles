9000bf5aa2f0cf400037a6fea8d458e6e9cdc6b4
smpl <sreddy@hey.com>
2025-09-12 09:57:54 -0400

    added cpp bits headers

---
+++ common/code/bits/dsa_codeforces.h
@@ -0,0 +1,18 @@
+#include "stdc++.h"
+using namespace std;
+
+// --- TYPE DEFINITIONS ----
+typedef long long ll;
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<pii> vpii;
+
+// Macros
+#define PB push_back
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define ff first
+#define ss second
---
+++ common/code/bits/dsa_general.h
@@ -0,0 +1,38 @@
+#include "stdc++.h"
+using namespace std;
+
+// --- TYPE DEFINITIONS ----
+typedef long long ll;
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
+typedef vector<pii> vpii;
+
+// --- MACROS ----
+#define PB push_back
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define ff first
+#define ss second
+
+void pvi(const vi& a) {
+    for (int num : a) cout << num << " ";
+    cout << endl;
+}
+
+void pvb(const vb& a) {
+    for (auto b : a) cout << b << " ";
+    cout << endl;
+}
+
+void pvp(const vpii& a) {
+    for (int i = 0; i < a.size(); i++) {
+        auto p = a[i];
+        if (i != 0) cout << ", ";
+        cout << "{" << p.first << "," << p.second << "}";
+        cout << endl;
+    }
+}
