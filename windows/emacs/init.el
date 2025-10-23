3ab34288ea8104731d0aa02b8d8505f615631bcb
shiva <shivajreddy@outlook.com>
2025-10-23 11:49:55 -0400

    Added perspective sessions to windows

--- windows/emacs/init.el
+++ windows/emacs/init.el
@@ -312,10 +312,11 @@
   :config
   (projectile-mode 1))
 
+;; Projectile commands under SPC p
 (smpl/leader-keys
-  "=" '(projectile-command-map :wk "Projectile commands"))
+  "p" '(projectile-command-map :wk "Projectile commands"))
 
-;; Perspective - workspace management
+;; Perspective - workspace management with named sessions
 (use-package perspective
   :ensure t
   :custom
@@ -323,7 +324,26 @@
   :init
   (persp-mode)
   :config
-  (setq persp-state-default-file "~/.config/emacs/sessions"))
+  (setq persp-state-default-file "~/.emacs.d/perspective-sessions")
+  ;; Auto-save perspectives on exit
+  (add-hook 'kill-emacs-hook #'persp-state-save))
+;; To restore: M-x persp-state-load or C-c M-p l
+
+;; Perspective commands under SPC =
+(smpl/leader-keys
+  "=" '(:ignore t :wk "Perspective")
+  "= =" '(persp-switch :wk "Switch perspective")
+  "= c" '(persp-switch :wk "Create/switch perspective")
+  "= k" '(persp-kill :wk "Kill perspective")
+  "= r" '(persp-rename :wk "Rename perspective")
+  "= n" '(persp-next :wk "Next perspective")
+  "= p" '(persp-prev :wk "Previous perspective")
+  "= s" '(persp-state-save :wk "Save all perspectives")
+  "= l" '(persp-state-load :wk "Load perspectives")
+  "= a" '(persp-add-buffer :wk "Add buffer to perspective")
+  "= A" '(persp-set-buffer :wk "Move buffer to perspective")
+  "= i" '(persp-import :wk "Import perspective")
+  "= I" '(persp-ibuffer :wk "IBuffer for perspective"))
 
 ;; Group buffers by perspective in ibuffer
 (add-hook 'ibuffer-hook
@@ -331,9 +351,6 @@
             (persp-ibuffer-set-filter-groups)
             (unless (eq ibuffer-sorting-mode 'alphabetic)
               (ibuffer-do-sort-by-alphabetic))))
-
-;; Auto-save perspective states on exit
-(add-hook 'kill-emacs-hook #'persp-state-save)
 
 ;;; ====================  VERSION CONTROL  ====================
 
