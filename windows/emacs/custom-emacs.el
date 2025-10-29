48f21ebc12cfd7abb567c140bc09609bf6511196
shiva <shivajreddy@outlook.com>
2025-10-29 08:38:43 -0400

    .

--- windows/emacs/custom-emacs.el
+++ windows/emacs/custom-emacs.el
@@ -4,7 +4,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(clang-format company-box counsel doom-modeline evil-collection
+                  evil-org format-all general golden-ratio
+                  gruber-darker-theme highlight-numbers
+                  highlight-symbol magit nerd-icons-dired
+                  nerd-icons-ivy-rich org-bullets org-modern
+                  perspective smartparens toc-org treemacs-projectile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
