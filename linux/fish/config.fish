6cf9e8f4e58e46a848f50e675fd5cd49badc3565
smpl <sreddy@hey.com>
2025-12-04 08:32:22 -0500

    updated waybar

--- linux/fish/config.fish
+++ linux/fish/config.fish
@@ -17,7 +17,6 @@ set -g fish_greeting ''
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    atuin init fish | source
 end
 
 ########################### PATHS ###########################
