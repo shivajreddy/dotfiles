dec81df0d9d27877b1853e7615fbdcd1f8aad797
smpl <sreddy@hey.com>
2026-01-04 12:42:19 -0500

    [git save]

--- windows/PowerShell/shiva.ps1
+++ windows/PowerShell/shiva.ps1
@@ -21,6 +21,10 @@ Set-Alias -Name gg -Value lazygit
 Set-Alias -Name y -Value yazi
 Set-Alias -Name c -Value cls
 Set-Alias -Name ff -Value fastfetch
+function wu {
+    winget update --all --include-unknown
+}
+
 
 Set-Alias -Name python -Value py
 Set-Alias -Name python3 -Value py
