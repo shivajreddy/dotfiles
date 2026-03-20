f3b9009f79eac3d71919681bb51510024ad238e5
smpl <sreddy@hey.com>
2026-03-20 08:48:27 -0400

    .

--- common/nvim/lua/plugins/conform.lua
+++ common/nvim/lua/plugins/conform.lua
@@ -5,6 +5,8 @@ return {
     opts = {
       formatters_by_ft = {
         python = { "black" }, -- or "ruff_format", "yapf", "autopep8"
+        json = { "prettierd", "prettier", stop_after_first = true },
+        jsonc = { "prettierd" },
       },
     },
   },
